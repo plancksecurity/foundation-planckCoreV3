70707b1da800780a45a2bc565353f793446ae559
vb <devnull@localhost>
2014-07-31 10:19:20 +0200

    removing byte order mark

--- src/update.sql
+++ src/update.sql
@@ -1,4 +1,4 @@
-﻿create table trust (
+create table trust (
    user_id text references person (id) on delete cascade,
    pgp_keypair_fpr text references pgp_keypair (fpr) on delete cascade,
    comm_type integer not null,
@@ -16,18 +16,17 @@ insert into trust (user_id, pgp_keypair_fpr, comm_type)
 alter table identity rename to identity_old;
 
 create table identity (
-	address text primary key,
-	user_id text
-		references person (id)
-		on delete cascade,
-	main_key_id text
-		references pgp_keypair (fpr)
-		on delete set null,
-	comment text
+    address text primary key,
+    user_id text
+        references person (id)
+        on delete cascade,
+    main_key_id text
+        references pgp_keypair (fpr)
+        on delete set null,
+    comment text
 );
 
 insert into identity (address, user_id, main_key_id)
     select address, user_id, main_key_id from identity_old;
 
 drop table identity_old;
-
