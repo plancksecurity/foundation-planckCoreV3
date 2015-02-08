93c7d160a93439a669796cba8e90685310b0089e
vb <devnull@localhost>
2015-02-08 12:01:58 -0500

    ...

--- src/pgp_gpg.h
+++ src/pgp_gpg.h
@@ -6,32 +6,32 @@ PEP_STATUS pgp_init(PEP_SESSION session);
 void pgp_release(PEP_SESSION session);
 
 PEP_STATUS pgp_decrypt_and_verify(
-    PEP_SESSION session, const char *ctext, size_t csize,
-    char **ptext, size_t *psize, stringlist_t **keylist
+        PEP_SESSION session, const char *ctext, size_t csize,
+        char **ptext, size_t *psize, stringlist_t **keylist
     );
 
 PEP_STATUS pgp_encrypt_and_sign(
-    PEP_SESSION session, const stringlist_t *keylist, const char *ptext,
-    size_t psize, char **ctext, size_t *csize
+        PEP_SESSION session, const stringlist_t *keylist, const char *ptext,
+        size_t psize, char **ctext, size_t *csize
     );
 
 PEP_STATUS pgp_verify_text(
-    PEP_SESSION session, const char *text, size_t size,
-    const char *signature, size_t sig_size, stringlist_t **keylist
+        PEP_SESSION session, const char *text, size_t size,
+        const char *signature, size_t sig_size, stringlist_t **keylist
     );
 
 PEP_STATUS pgp_delete_keypair(PEP_SESSION session, const char *fpr);
 
 PEP_STATUS pgp_export_key(
-    PEP_SESSION session, const char *fpr, char **key_data, size_t *size
+        PEP_SESSION session, const char *fpr, char **key_data, size_t *size
     );
 
 PEP_STATUS pgp_find_keys(
-    PEP_SESSION session, const char *pattern, stringlist_t **keylist
+        PEP_SESSION session, const char *pattern, stringlist_t **keylist
     );
 
 PEP_STATUS pgp_generate_keypair(
-    PEP_SESSION session, pEp_identity *identity
+        PEP_SESSION session, pEp_identity *identity
     );
 
 PEP_STATUS pgp_get_key_rating(
@@ -40,8 +40,10 @@ PEP_STATUS pgp_get_key_rating(
     PEP_comm_type *comm_type
     );
 
-PEP_STATUS pgp_import_key(PEP_SESSION session, const char *key_data, size_t size);
+PEP_STATUS pgp_import_key(PEP_SESSION session, const char *key_data,
+        size_t size);
 
 PEP_STATUS pgp_recv_key(PEP_SESSION session, const char *pattern);
 
 PEP_STATUS pgp_send_key(PEP_SESSION session, const char *pattern);
+
