b2f68c6b07c9909d8dd98608c4e4fa27087c79e4
Volker Birk <vb@pep-project.org>
2015-10-05 01:11:59 +0200

    IDs should be, well, IDs

--- db/create_system_db.sql
+++ db/create_system_db.sql
@@ -16,5 +16,5 @@ CREATE TABLE i18n (
 CREATE UNIQUE INDEX i18n_pk on i18n (lang, id);
 
 INSERT INTO i18n VALUES ('en', 1, 'I want to have this conversation in English language');
-INSERT INTO i18n VALUES ('de', 1, 'Ich möchte diese Unterhaltung auf Deutsch führen');
+INSERT INTO i18n VALUES ('de', 2, 'Ich möchte diese Unterhaltung auf Deutsch führen');
 ' add more languages here
