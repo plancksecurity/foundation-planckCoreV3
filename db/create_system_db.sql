b9f68d4defb3684412a6bef03d1fd044299c33d3
Volker Birk <vb@pep-project.org>
2015-10-05 01:41:28 +0200

    DDL needs no ID for languages

--- db/create_system_db.sql
+++ db/create_system_db.sql
@@ -8,13 +8,10 @@ CREATE TABLE wordlist (
 CREATE UNIQUE INDEX wordlist_pk on wordlist (lang, id);
 
 CREATE TABLE i18n (
-    lang text,
-    id integer,
+    lang text primary key,
     phrase text
 );
 
-CREATE UNIQUE INDEX i18n_pk on i18n (lang, id);
-
-INSERT INTO i18n VALUES ('en', 1, 'I want to have this conversation in English language');
-INSERT INTO i18n VALUES ('de', 2, 'Ich möchte diese Unterhaltung auf Deutsch führen');
+INSERT INTO i18n VALUES ('en', 'I want to have this conversation in English language');
+INSERT INTO i18n VALUES ('de', 'Ich möchte diese Unterhaltung auf Deutsch führen');
 ' add more languages here
