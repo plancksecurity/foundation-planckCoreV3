183a770648c9e94d066900632dbb5c4ea25eace0
Volker Birk <vb@pep-project.org>
2016-02-21 22:48:35 +0100

    unittest file

---
+++ test/i18n_test.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include "pEpEngine.h"
+
+using namespace std;
+
+int main() {
+    cout << "\n*** i18n_test ***\n\n";
+
+    PEP_SESSION session;
+    
+    cout << "calling init()\n";
+    PEP_STATUS status1 = init(&session);   
+    assert(status1 == PEP_STATUS_OK);
+    assert(session);
+    cout << "init() completed.\n";
+
+    // i18n test code
+
+    char *languages;
+    PEP_STATUS status2 = get_languagelist(session, &languages);
+    assert(status2 == PEP_STATUS_OK);
+    assert(languages);
+
+    cout << languages;
+    pEp_free(languages);
+
+    char *phrase;
+    PEP_STATUS status3 = get_phrase(session, "de", 1, &phrase);
+    assert(status3 == PEP_STATUS_OK);
+    assert(phrase);
+
+    cout << "\nGerman: " << phrase << "\n";
+    pEp_free(phrase);
+
+    cout << "calling release()\n";
+    release(session);
+    return 0;
+}
+
