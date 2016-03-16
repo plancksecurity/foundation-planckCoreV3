575718b1136c0fdd5859e9e72ea83091f5e48d53
Volker Birk <vb@pep-project.org>
2016-03-16 11:35:12 +0100

    adding test

---
+++ test/pgp_binary_test.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include "message_api.h"
+
+using namespace std;
+
+int main() {
+    cout << "\n*** pgp_binary_test ***\n\n";
+
+    PEP_SESSION session;
+    
+    cout << "calling init()\n";
+    PEP_STATUS status1 = init(&session);   
+    assert(status1 == PEP_STATUS_OK);
+    assert(session);
+    cout << "init() completed.\n";
+
+    // pgp_binary test code
+
+    const char *path;
+    PEP_STATUS status2 = get_binary_path(PEP_crypt_OpenPGP, &path);
+    assert(status2 == PEP_STATUS_OK);
+    if (path)
+        cout << "PGP binary at " << path << "\n";
+    else
+        cout << "no PGP binary path available\n";
+
+    cout << "calling release()\n";
+    release(session);
+    return 0;
+}
+
