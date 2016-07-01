88e066f3a685b6b7313d6df4496fc84294fee5b1
Volker Birk <vb@pep-project.org>
2016-07-01 08:52:00 +0200

    ...

--- src/sync_fsm.h
+++ src/sync_fsm.h
@@ -68,7 +68,7 @@ DeviceState_state fsm_DeviceState(
 
 // driver
 
-PEP_STATUS fsm_DeviceState_inject(
+DYNAMIC_API PEP_STATUS fsm_DeviceState_inject(
         PEP_SESSION session,
         DeviceState_event event,
         Identity partner,
