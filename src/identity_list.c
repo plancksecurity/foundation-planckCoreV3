6c59b05c2c7c1d04ca70fcfec8f793c080adf52a
Krista Grothoff <krista@pep-project.org>
2016-07-17 10:12:33 +0200

    fix #27: identity_list_add now iterative

--- src/identity_list.c
+++ src/identity_list.c
@@ -61,17 +61,26 @@ DYNAMIC_API identity_list *identity_list_add(identity_list *id_list, pEp_identit
     if (id_list == NULL)
         return new_identity_list(ident);
 
+    // empty list
     if (id_list->ident == NULL) {
+        if (id_list->next)
+            return NULL; // invalid list
+            
         id_list->ident = ident;
+        
+        if (id_list->ident == NULL)
+            return NULL;
+        
         return id_list;
     }
-    else if (id_list->next == NULL) {
-        id_list->next = new_identity_list(ident);
-        return id_list->next;
-    }
-    else {
-        return identity_list_add(id_list->next, ident);
-    }
+ 
+    identity_list* list_curr = id_list;
+    while (list_curr->next)
+        list_curr = list_curr->next;
+    
+    list_curr->next = new_identity_list(ident);
+    
+    return list_curr->next;
 }
 
 DYNAMIC_API int identity_list_length(const identity_list *id_list)
