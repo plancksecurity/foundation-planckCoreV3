fcf368b8f1cd4b2ae939c32b97baa120060b0858
Volker Birk <vb@pep.foundation>
2016-08-08 00:06:59 +0200

    hm... without_double_ending() was correct ;-)

--- src/message_api.c
+++ src/message_api.c
@@ -739,7 +739,7 @@ static char * without_double_ending(const char *filename)
     if (ext == NULL)
         return NULL;
 
-    char *result = strndup(filename, ext - filename + 1);
+    char *result = strndup(filename, ext - filename);
     assert(result);
     return result;
 }
