24c1f7917a34a6d3e3f3ec520c6fd0dd395bcbb3
Krista Grothoff <krista@pep-project.org>
2016-08-22 19:10:41 +0200

    Documentation bug.

--- src/openpgp_compat.h
+++ src/openpgp_compat.h
@@ -33,11 +33,8 @@ extern "C" {
 //                            be listed; false if only valid keys should be shown
 //      keyinfo_list (out)    list of identities for each available key 
 //
-//  caveat: FIXME
-//        the ownership of the identity list goes to the caller
-//        the caller must use free_identity_list() to free it
-//        identity objects derived from the keyring only have the available information
-//           from the keyring; some fields may be NULL
+//  caveat:
+//      keyinfo_list must be freed by the caller.
 DYNAMIC_API PEP_STATUS OpenPGP_list_keyinfo (
         PEP_SESSION session, const char* search_pattern, stringpair_list_t** keyinfo_list
     );
