21acdf33fa9200b2e2d32a3ce73374e478600ce9
Krista Grothoff <krista@pep-project.org>
2016-10-07 11:29:56 +0200

    Actually, the flag was correct. Sigh.

--- src/keymanagement.c
+++ src/keymanagement.c
@@ -171,7 +171,7 @@ DYNAMIC_API PEP_STATUS update_identity(
     if (stored_identity) {
         PEP_comm_type _comm_type_key;
         
-        bool dont_use_fpr = false;
+        bool dont_use_fpr = true;
 
         /* if we have a stored_identity fpr */
         if (!EMPTYSTR(stored_identity->fpr)) {
