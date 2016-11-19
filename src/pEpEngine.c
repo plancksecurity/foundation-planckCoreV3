b80188214af17783067fc92b986adca15eee3ebb
Edouard Tisserant <edouard@pep-project.org>
2016-11-19 01:06:50 +0100

    ENGINE-140 #comment more fixes about identity.flags in SQLite statements

--- src/pEpEngine.c
+++ src/pEpEngine.c
@@ -410,12 +410,13 @@ DYNAMIC_API PEP_STATUS init(PEP_SESSION *session)
                             "   join pgp_keypair on fpr = identity.main_key_id"
                             "   join trust on id = trust.user_id"
                             "       and pgp_keypair_fpr = identity.main_key_id"
-                            "   where identity.user_id = '" PEP_OWN_USERID "';";
+                            "   where identity.user_id = '" PEP_OWN_USERID "'"
+                            "       and (identity.flags & 1) = 0;";
         
         sql_own_keys_retrieve =  
                             "select fpr from own_keys"
                             "   join identity"
-                            "   where identity.flags & 1 = 0;"; // PEP_idf_not_for_sync
+                            "   where (identity.flags & 1) = 0;"; // PEP_idf_not_for_sync
 
         sql_set_own_key = "insert or replace into own_keys (address, user_id, fpr)"
                           " values (?1, '" PEP_OWN_USERID "', upper(replace(?2,' ','')));";
