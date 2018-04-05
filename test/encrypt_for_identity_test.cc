03b1a166a52ad650121a13a63485a2d8ec8340da
Krista Bennett <krista@pep-project.org>
2018-04-05 16:13:14 +0200

    ENGINE-416: test fixes, warts and all

--- test/encrypt_for_identity_test.cc
+++ test/encrypt_for_identity_test.cc
@@ -29,18 +29,31 @@ int main() {
 
     const string alice_pub_key = slurp("test_keys/pub/pep-test-alice-0x6FF00E97_pub.asc");
     const string alice_priv_key = slurp("test_keys/priv/pep-test-alice-0x6FF00E97_priv.asc");
+    const string gabrielle_pub_key = slurp("test_keys/pub/pep-test-gabrielle-0xE203586C_pub.asc");
+    const string bella_pub_key = slurp("test_keys/pub/pep.test.bella-0xAF516AAE_pub.asc");    
 
     PEP_STATUS statuspub = import_key(session, alice_pub_key.c_str(), alice_pub_key.length(), NULL);
     PEP_STATUS statuspriv = import_key(session, alice_priv_key.c_str(), alice_priv_key.length(), NULL);
     assert(statuspub == PEP_STATUS_OK);
     assert(statuspriv == PEP_STATUS_OK);
+    
+    statuspub = import_key(session, gabrielle_pub_key.c_str(), gabrielle_pub_key.length(), NULL);
+    assert(statuspub == PEP_STATUS_OK);
+    statuspub = import_key(session, bella_pub_key.c_str(), bella_pub_key.length(), NULL);
+    assert(statuspub == PEP_STATUS_OK);
+
+    const char* alice_fpr = "4ABE3AAF59AC32CFE4F86500A9411D176FF00E97";
+    const char* gabrielle_fpr = "906C9B8349954E82C5623C3C8C541BD4E203586C";
+    const char* bella_fpr = "5631BF1357326A02AA470EEEB815EF7FA4516AAE";
+    const char* nobody_fpr = "1111111111111111111111111111111111111111";
 
     cout << "creating message…\n";
-    pEp_identity* alice = new_identity("pep.test.alice@pep-project.org", "4ABE3AAF59AC32CFE4F86500A9411D176FF00E97", PEP_OWN_USERID, "Alice Test");
+    pEp_identity* alice = new_identity("pep.test.alice@pep-project.org", alice_fpr, PEP_OWN_USERID, "Alice Test");
     pEp_identity* bob = new_identity("pep.test.bob@pep-project.org", NULL, "42", "Bob Test");
+    
     alice->me = true;
 
-    PEP_STATUS mystatus = set_own_key(session, alice, "4ABE3AAF59AC32CFE4F86500A9411D176FF00E97");
+    PEP_STATUS mystatus = set_own_key(session, alice, alice_fpr);
     assert(mystatus == PEP_STATUS_OK);
 
     identity_list* to_list = new_identity_list(bob); // to bob
@@ -63,7 +76,6 @@ int main() {
 
     free(encoded_text);
 
-    cout << "encrypting message as MIME multipart…\n";
     message* encrypted_msg = nullptr;
     cout << "calling encrypt_message_for_identity()\n";
     status = encrypt_message_for_self(session, alice, outgoing_message, NULL, &encrypted_msg, PEP_enc_PGP_MIME, PEP_encrypt_flag_force_unsigned | PEP_encrypt_flag_force_no_attached_key);
@@ -123,8 +135,84 @@ int main() {
     cout << "freeing messages…\n";
     free_message(encrypted_msg);
     free_message(decrypted_msg);
-    free_message(outgoing_message);
+    free_stringlist (keylist_used);
     cout << "done.\n";
+
+    cout << "Now encrypt for self with extra keys." << endl;
+    stringlist_t* extra_keys = new_stringlist(gabrielle_fpr);
+    stringlist_add(extra_keys, bella_fpr);
+    encrypted_msg = NULL;
+    decrypted_msg = NULL;
+    keylist_used = NULL;
+
+    cout << "calling encrypt_message_for_identity()\n";
+    status = encrypt_message_for_self(session, alice, outgoing_message, extra_keys, &encrypted_msg, PEP_enc_PGP_MIME, PEP_encrypt_flag_force_unsigned | PEP_encrypt_flag_force_no_attached_key);
+    cout << "encrypt_message() returns " << std::hex << status << '.' << endl;
+    assert(status == PEP_STATUS_OK);
+    assert(encrypted_msg);
+    cout << "message encrypted.\n";
+    
+    status = decrypt_message(session, encrypted_msg, &decrypted_msg, &keylist_used, &rating, &flags);
+    assert(decrypted_msg);
+    assert(keylist_used);
+    assert(rating);
+    assert(status == PEP_DECRYPTED && rating == PEP_rating_unreliable);
+    ct = encrypted_msg->from->comm_type;
+    assert(ct == PEP_ct_pEp || ct == PEP_ct_pEp_unconfirmed || ct == PEP_ct_OpenPGP || ct == PEP_ct_OpenPGP_unconfirmed );
+
+    cout << "keys used:\n";
+
+    for (stringlist_t* incoming_kl = extra_keys; incoming_kl && incoming_kl->value; incoming_kl = incoming_kl->next) {
+        bool found = false;
+        cout << "Encrypted for: ";
+        for (stringlist_t* kl4 = keylist_used; kl4 && kl4->value; kl4 = kl4->next, i++) {
+            if (strcasecmp(incoming_kl->value, kl4->value) == 0) {
+                cout << "\t " << kl4->value;
+                found = true;
+                break;
+            }
+        }
+        cout << endl;
+        assert(found);
+    }
+    cout << "Encrypted for all the extra keys!" << endl;
+
+    bool found = false;
+    for (stringlist_t* kl4 = keylist_used; kl4 && kl4->value; kl4 = kl4->next)
+    {
+        if (strcasecmp(alice_fpr, kl4->value) == 0) {
+            found = true;
+            cout << "Encrypted also for Alice! Yay!" << endl;
+            break;
+        }
+    }
+    assert(found);
+
+    free_message(encrypted_msg);
+    encrypted_msg = NULL;
+    free_message(decrypted_msg);
+    decrypted_msg = NULL;
+    free_stringlist(keylist_used);
+    keylist_used = NULL;
+
+    cout << "Now add a bad fpr." << endl;
+    
+    stringlist_add(extra_keys, nobody_fpr);
+    
+    cout << "calling encrypt_message_for_identity()\n";
+    status = encrypt_message_for_self(session, alice, outgoing_message, extra_keys, &encrypted_msg, PEP_enc_PGP_MIME, PEP_encrypt_flag_force_unsigned | PEP_encrypt_flag_force_no_attached_key);
+    cout << "encrypt_message() returns " << std::hex << status << '.' << endl;
+    assert(status != PEP_STATUS_OK);
+
+    free_message(outgoing_message);
+    outgoing_message = NULL;
+    free_message(encrypted_msg);
+    encrypted_msg = NULL;
+    free_message(decrypted_msg);
+    decrypted_msg = NULL;
+    free_stringlist(keylist_used);
+    keylist_used = NULL;
+
 
     cout << "*** Now testing MIME_encrypt_for_self ***" << endl;
 
@@ -166,7 +254,7 @@ int main() {
                                   &mimerating,
                                   &mimeflags);
 
-    assert(decrypted_msg);
+    assert(decrypted_mimetext);
     assert(keylist_used);
     assert(mimerating);
                              
