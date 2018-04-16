d8270aed12ab2fa70eb40b60163919be162c6ef2
Krista Bennett <krista@pep-project.org>
2018-04-16 12:21:02 +0200

    ENGINE-420: added forgotten test

---
+++ test/decrypt_attach_private_key_trusted_test.cc
@@ -0,0 +1,144 @@
+// This file is under GNU General Public License 3.0
+// see LICENSE.txt
+
+#include <stdlib.h>
+#include <string.h>
+#include "platform.h"
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include "mime.h"
+#include "message_api.h"
+#include "keymanagement.h"
+#include "test_util.h"
+
+using namespace std;
+
+int main() {
+    cout << "\n*** decrypt_attach_private_key_trusted_test ***\n\n";
+
+    PEP_SESSION session;
+    
+    cout << "calling init()\n";
+    PEP_STATUS status1 = init(&session);
+    assert(status1 == PEP_STATUS_OK);
+    assert(session);
+    cout << "init() completed.\n";
+
+    const char* own_uid = PEP_OWN_USERID;
+
+    cout << "Importing keys..." << endl;
+    
+    string input_key;
+    const char* main_addr = "priv-key-import-test-main@darthmama.cool";
+    pEp_identity* main_me = NULL;
+    const char* fpr_main_me = "13A9F97964A2B52520CAA40E51BCA783C065A213";    
+    pEp_identity* same_addr_same_uid = NULL;
+    const char* fpr_same_addr_same_uid = "8AB616A3BD51DEF714B5E688EFFB540C3276D2E5";
+        
+    PEP_STATUS status = PEP_STATUS_OK;
+
+    // key for main own user
+    // 
+    // 13A9F97964A2B52520CAA40E51BCA783C065A213    
+    input_key = slurp("test_keys/pub/priv-key-import-test-main_0-0xC065A213_pub.asc");
+    status = import_key(session, input_key.c_str(), input_key.length(), NULL);
+    assert(status == PEP_STATUS_OK);
+
+    input_key = slurp("test_keys/priv/priv-key-import-test-main_0-0xC065A213_priv.asc");
+    status = import_key(session, input_key.c_str(), input_key.length(), NULL);
+    assert(status == PEP_STATUS_OK);
+
+    // ensure there's no private key
+    bool has_priv = false;
+    status = contains_priv_key(session, fpr_same_addr_same_uid, &has_priv);
+    if (status == PEP_STATUS_OK && has_priv) {
+        cout << "SORRY, have to delete keys here to run test correctly..." << endl;
+        status = delete_keypair(session, fpr_same_addr_same_uid);
+        if (status == PEP_STATUS_OK) {
+            has_priv = false;
+            status = contains_priv_key(session, fpr_same_addr_same_uid, &has_priv);
+            assert(has_priv == false);
+            cout << "Successfully deleted keypair for " << fpr_same_addr_same_uid << " - will now import the public key only" << endl;
+        }
+        else
+            cout << "Warning - delete keypair returned status " << tl_status_string(status) << ". This may or may not be an error, depending on what you expect." << endl;            
+    }
+        
+    // key with same address and user_id
+    // 8AB616A3BD51DEF714B5E688EFFB540C3276D2E5
+    input_key = slurp("test_keys/pub/priv-key-import-test-main_0-0x3276D2E5_pub.asc");
+    status = import_key(session, input_key.c_str(), input_key.length(), NULL);
+    assert(status == PEP_STATUS_OK);
+
+    
+    cout << "Setting up own identity with default key " << fpr_main_me << endl;
+    // Own identity with default key etc
+    main_me = new_identity(main_addr, fpr_main_me, own_uid, "PrivateKey Import Test");
+    status = set_own_key(session, main_me, fpr_main_me);
+    assert(status == PEP_STATUS_OK);
+
+    assert(strcmp(main_me->fpr, fpr_main_me) == 0);
+    cout << "Done!" << endl << endl;
+    
+    cout << "Setting up sender identities and resetting key trust." << endl;
+    cout << "Same address, same user_id - address: " << main_addr << ", user_id: " << own_uid << ", fpr: " << fpr_same_addr_same_uid << endl;  
+    same_addr_same_uid = new_identity(main_addr, fpr_same_addr_same_uid, own_uid, "PrivateKey Import Test");
+    assert(status == PEP_STATUS_OK || status == PEP_CANNOT_FIND_IDENTITY);
+    assert((same_addr_same_uid->comm_type & PEP_ct_confirmed) != PEP_ct_confirmed);
+
+    status = key_reset_trust(session, same_addr_same_uid);
+    
+    cout << "Done!" << endl << endl;
+
+    cout << "Reading in message..." << endl;
+    
+    string encoded_text = slurp("test_mails/priv_key_attach.eml");
+
+    cout << "Starting test..." << endl;
+    // Case 1:
+    // Same address, same user_id, untrusted
+    cout << "decrypt with attached private key: Same address, same user_id, trusted" << endl;
+    char* decrypted_text = NULL;
+    stringlist_t* keylist_used = NULL;
+    PEP_rating rating;
+    PEP_decrypt_flags_t flags;
+    
+    status = trust_personal_key(session, same_addr_same_uid);
+    assert(status == PEP_STATUS_OK);
+    free(decrypted_text);
+    decrypted_text = NULL;
+
+    status = get_trust(session, same_addr_same_uid);
+    cout << tl_ct_string(same_addr_same_uid->comm_type) << endl;
+    
+    assert(same_addr_same_uid->comm_type == PEP_ct_pEp);
+    
+    status = MIME_decrypt_message(session, encoded_text.c_str(), 
+                                  encoded_text.size(), &decrypted_text, 
+                                  &keylist_used, &rating, &flags);
+
+    status = get_trust(session, same_addr_same_uid);
+    assert(same_addr_same_uid->comm_type == PEP_ct_pEp);
+    
+    status = MIME_decrypt_message(session, encoded_text.c_str(), 
+                                  encoded_text.size(), &decrypted_text, 
+                                  &keylist_used, &rating, &flags);
+    
+    cout << "Status: " << tl_status_string(status) << endl;
+    assert(status == PEP_STATUS_OK);
+
+    cout << decrypted_text << endl;
+    
+    has_priv = false;
+    status = contains_priv_key(session, fpr_same_addr_same_uid, &has_priv);
+    assert(has_priv == true);
+    cout << "Private key was also imported." << endl;
+    
+    cout << "PASS!" << endl;
+    status = key_reset_trust(session, main_me);      
+    status = key_reset_trust(session, same_addr_same_uid);      
+    release(session);
+    
+    return 0;
+}
