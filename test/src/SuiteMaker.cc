40174bb3bf4cd009db0c62a669f36bd7421d19e7
Krista Bennett <krista@pep-project.org>
2018-05-02 16:59:14 +0200

    working on autogen of test suite object builders

--- test/src/SuiteMaker.cc
+++ test/src/SuiteMaker.cc
@@ -1,3 +1,5 @@
+// This file is under GNU General Public License 3.0
+// see LICENSE.txt
 #include <cpptest.h>
 #include <cpptest-suite.h>
 #include <cpptest-textoutput.h>
@@ -9,14 +11,21 @@
 #include "EngineTestSessionSuite.h"
 #include "SuiteMaker.h"
 
-// Begin where we should generate stuff
+// Begin where we generate stuff
 #include "DecorateTests.h"
+#include "AppleMailTests.h"
+
 
 using namespace std;
+
+static const char* const all_suites[] = {
+    "DecorateTests",
+    "AppleMailTests",
+};
 
 void suitemaker_build(const char* test_class_name, const char* test_home, std::auto_ptr<Test::Suite>& test_suite) {
     if (strcmp(test_class_name, "DecorateTests") == 0)
         test_suite = auto_ptr<Test::Suite> (new DecorateTests(test_class_name, test_home));
     else if (strcmp(test_class_name, "AppleMailTests") == 0)
-        test_suite = auto_ptr<Test::Suite> (new DecorateTests(test_class_name, test_home));    
+        test_suite = auto_ptr<Test::Suite> (new AppleMailTests(test_class_name, test_home));
 }
