d1a3a423fb6f368754a8c24f5145cba4de2acb9c
Krista Bennett <krista@pep-project.org>
2018-10-08 13:16:37 +0200

    Except that 441 WAS the last version. Now 442. Releasing as patch release for Claudio.

--- src/pEp_internal.h
+++ src/pEp_internal.h
@@ -1,7 +1,7 @@
 // This file is under GNU General Public License 3.0
 // see LICENSE.txt
 
-#define PEP_ENGINE_VERSION "1.0.441"
+#define PEP_ENGINE_VERSION "1.0.442"
 
 // maximum attachment size to import as key 1MB, maximum of 20 attachments
 
