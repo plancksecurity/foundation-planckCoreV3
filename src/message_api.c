c863f370b9902cffecd9ac2b489db6b672c97afc
Volker Birk <vb@pep.foundation>
2018-10-30 13:58:30 +0100

    fixing bug when message unencrypted

--- src/message_api.c
+++ src/message_api.c
@@ -3715,8 +3715,10 @@ DYNAMIC_API PEP_STATUS decrypt_message(
 {
     PEP_STATUS status = _decrypt_message(session, src, dst, keylist, rating, flags, NULL);
 
-    if (session->inject_sync_event && *dst && (*dst)->attachments) {
-        for (bloblist_t *bl = (*dst)->attachments; bl ; bl = bl->next) {
+    message *msg = *dst ? *dst : src;
+
+    if (session->inject_sync_event && msg && msg->attachments) {
+        for (bloblist_t *bl = msg->attachments; bl ; bl = bl->next) {
             if (bl->mime_type && strcasecmp(bl->mime_type, "application/pEp.sync") == 0)
                 signal_Sync_message(session, *rating, bl->value, bl->size);
         }
