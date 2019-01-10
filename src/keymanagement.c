64dc736e2164141dbde240ea2693c6d3515f57f7
Krista 'DarthMama' Bennett <krista@pep.foundation>
2019-01-10 18:57:53 +0100

    Myself was returning OK, even when it needed to and could not generate a key. Fixed.

--- src/keymanagement.c
+++ src/keymanagement.c
@@ -1104,9 +1104,13 @@ PEP_STATUS _myself(PEP_SESSION session, pEp_identity * identity, bool do_keygen,
         identity->comm_type = PEP_ct_unknown;
     }
     
-    status = set_identity(session, identity);
-    if (status == PEP_STATUS_OK)
-        status = set_as_pep_user(session, identity);
+    // We want to set an identity in the DB even if a key isn't found, but we have to preserve the status if
+    // it's NOT ok
+    PEP_STATUS set_id_status = set_identity(session, identity);
+    if (set_id_status == PEP_STATUS_OK)
+        set_id_status = set_as_pep_user(session, identity);
+
+    status = (status == PEP_STATUS_OK ? set_id_status : status);
 
 pep_free:    
     free(default_own_id);
