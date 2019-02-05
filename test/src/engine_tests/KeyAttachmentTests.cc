f9d98997798364454a4550998b5030c324d03e08
Krista 'DarthMama' Bennett <krista@pep.foundation>
2019-02-05 10:30:27 +0100

    ENGINE-448: additional tests, and branching to fix ENGINE-510

--- test/src/engine_tests/KeyAttachmentTests.cc
+++ test/src/engine_tests/KeyAttachmentTests.cc
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string>
 #include <cstring>
+#include <vector>
+#include <utility>
 #include <cassert>
 
 #include "pEpEngine.h"
@@ -118,7 +120,29 @@ void KeyAttachmentTests::check_key_plus_encr_att_inline() {
 }
 
 void KeyAttachmentTests::check_encr_att_plus_key_inline() {
-    TEST_ASSERT(true);
+    string msg = slurp("test_mails/Inline PGP Test - encr file then key.eml");
+    message* enc_msg = NULL;
+    message* dec_msg = NULL;
+
+    PEP_STATUS status = mime_decode_message(msg.c_str(), msg.size(), &enc_msg);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));
+    TEST_ASSERT(enc_msg);
+    stringlist_t* keylist = NULL;
+    PEP_rating rating;
+    PEP_decrypt_flags_t flags = 0;
+    status = decrypt_message(session, enc_msg, &dec_msg, &keylist, &rating, &flags);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));    
+    TEST_ASSERT(dec_msg);
+    TEST_ASSERT_MSG(dec_msg->attachments, "Encrypted attachment not preserved.");
+    TEST_ASSERT_MSG(dec_msg->attachments->next == NULL, "Decryption left attachments it should have deleted.");    
+    TEST_ASSERT_MSG(dec_msg->attachments->filename, "Attachment doesn't have a filename");
+    // TODO: is there a missing update to resource IDs in decrypt in parts?
+    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->filename, "cheese.txt.gpg") == 0, dec_msg->attachments->filename);    
+    TEST_ASSERT_MSG(dec_msg->attachments->mime_type, "Attachment doesn't have a mime type");
+    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->mime_type, "application/octet-stream") == 0, dec_msg->attachments->mime_type);    
+    free_message(enc_msg);
+    free_message(dec_msg);
+    free_stringlist(keylist);
 }
 
 void KeyAttachmentTests::check_key_plus_unencr_att_inline() {
@@ -148,8 +172,34 @@ void KeyAttachmentTests::check_key_plus_unencr_att_inline() {
 }
 
 void KeyAttachmentTests::check_unencr_att_plus_key_inline() {
-    TEST_ASSERT(true);
+    string msg = slurp("test_mails/Inline PGP Test - unencr file then key.eml");
+    message* enc_msg = NULL;
+    message* dec_msg = NULL;
+
+    PEP_STATUS status = mime_decode_message(msg.c_str(), msg.size(), &enc_msg);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));
+    TEST_ASSERT(enc_msg);
+    stringlist_t* keylist = NULL;
+    PEP_rating rating;
+    PEP_decrypt_flags_t flags = 0;
+    status = decrypt_message(session, enc_msg, &dec_msg, &keylist, &rating, &flags);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));    
+    TEST_ASSERT(dec_msg);
+    TEST_ASSERT_MSG(dec_msg->attachments, "Encrypted attachment not preserved.");
+    TEST_ASSERT_MSG(dec_msg->attachments->next == NULL, "Decryption left attachments it should have deleted.");    
+    TEST_ASSERT_MSG(dec_msg->attachments->filename, "Attachment doesn't have a filename");
+    // TODO: is there a missing update to resource IDs in decrypt in parts?
+    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->filename, "cheese.txt") == 0, dec_msg->attachments->filename);    
+    TEST_ASSERT_MSG(dec_msg->attachments->mime_type, "Attachment doesn't have a mime type");
+    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->mime_type, "application/octet-stream") == 0, dec_msg->attachments->mime_type);    
+    free_message(enc_msg);
+    free_message(dec_msg);
+    free_stringlist(keylist);
 }
+
+// Inline PGP - many keys with many files.eml
+// OpenPGP test - many keys and many files.eml        
+
 
 void KeyAttachmentTests::check_many_keys_inline() {
     string msg = slurp("test_mails/Inline PGP test - many keys.eml");
@@ -189,7 +239,7 @@ void KeyAttachmentTests::check_many_keys_w_encr_file_inline() {
     TEST_ASSERT_MSG(dec_msg->attachments->next == NULL, "Decryption left attachments it should have deleted.");    
     TEST_ASSERT_MSG(dec_msg->attachments->filename, "Attachment doesn't have a filename");
     // TODO: is there a missing update to resource IDs in decrypt in parts?
-    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->filename, "cheese.txt") == 0, dec_msg->attachments->filename);    
+    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->filename, "cheese.txt.gpg") == 0, dec_msg->attachments->filename);    
     TEST_ASSERT_MSG(dec_msg->attachments->mime_type, "Attachment doesn't have a mime type");
     TEST_ASSERT_MSG(strcmp(dec_msg->attachments->mime_type, "application/octet-stream") == 0, dec_msg->attachments->mime_type);    
     free_message(enc_msg);
@@ -198,7 +248,29 @@ void KeyAttachmentTests::check_many_keys_w_encr_file_inline() {
 }
         
 void KeyAttachmentTests::check_many_keys_w_unencr_file_inline() {
-    TEST_ASSERT(true);
+    string msg = slurp("test_mails/Inline PGP Test - many keys unencr file in middle.eml");
+    message* enc_msg = NULL;
+    message* dec_msg = NULL;
+
+    PEP_STATUS status = mime_decode_message(msg.c_str(), msg.size(), &enc_msg);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));
+    TEST_ASSERT(enc_msg);
+    stringlist_t* keylist = NULL;
+    PEP_rating rating;
+    PEP_decrypt_flags_t flags = 0;
+    status = decrypt_message(session, enc_msg, &dec_msg, &keylist, &rating, &flags);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));    
+    TEST_ASSERT(dec_msg);
+    TEST_ASSERT_MSG(dec_msg->attachments, "Encrypted attachment not preserved.");
+    TEST_ASSERT_MSG(dec_msg->attachments->next == NULL, "Decryption left attachments it should have deleted.");    
+    TEST_ASSERT_MSG(dec_msg->attachments->filename, "Attachment doesn't have a filename");
+    // TODO: is there a missing update to resource IDs in decrypt in parts?
+    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->filename, "barky.txt") == 0, dec_msg->attachments->filename);    
+    TEST_ASSERT_MSG(dec_msg->attachments->mime_type, "Attachment doesn't have a mime type");
+    TEST_ASSERT_MSG(strcmp(dec_msg->attachments->mime_type, "application/octet-stream") == 0, dec_msg->attachments->mime_type);    
+    free_message(enc_msg);
+    free_message(dec_msg);
+    free_stringlist(keylist);
 }
 
 void KeyAttachmentTests::check_key_attach_OpenPGP() {
@@ -393,5 +465,57 @@ void KeyAttachmentTests::check_many_keys_w_unencr_file_OpenPGP() {
 }
          
 void KeyAttachmentTests::check_many_keys_w_many_files_OpenPGP() {
-    TEST_ASSERT(true);
+    string msg = slurp("test_mails/OpenPGP test - many keys and many files.eml");
+    message* enc_msg = NULL;
+    message* dec_msg = NULL;
+
+    PEP_STATUS status = mime_decode_message(msg.c_str(), msg.size(), &enc_msg);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));
+    TEST_ASSERT(enc_msg);
+    stringlist_t* keylist = NULL;
+    PEP_rating rating;
+    PEP_decrypt_flags_t flags = 0;
+    status = decrypt_message(session, enc_msg, &dec_msg, &keylist, &rating, &flags);
+    TEST_ASSERT_MSG(status == PEP_STATUS_OK, tl_status_string(status));    
+    TEST_ASSERT(dec_msg);
+
+    const char* not_pres = "Encrypted attachment not preserved.";
+    const char* left_att = "Decryption left attachments it should have deleted.";
+    const char* no_fname = "Attachment doesn't have a filename.";
+    const char* no_mime = "Attachment doesn't have a MIME type.";
+
+    // pair is filename, mime_type 
+    vector<pair<string,string>> v =
+        {
+            {"file://index.html","text/html"},
+            {"file://barky.txt","text/plain"}, 
+            {"file://cheese.txt.gpg","application/octet-stream"},
+            {"file://this_is_not_a_key_or_encrypted.asc","text/plain"},
+            {"file://this_is_not_a_key_or_encrypted.gpg","text/plain"},
+            {"file://cheese.txt","text/plain"}
+        };
+                             
+    bloblist_t* curr_att = dec_msg->attachments;
+    vector<pair<string,string>>::iterator it = v.begin();
+
+    int i = 0;
+    
+    while (it != v.end()) {
+        cout << i++ << endl; 
+        TEST_ASSERT_MSG(curr_att, not_pres);
+        TEST_ASSERT_MSG(curr_att->filename, no_fname);
+        TEST_ASSERT_MSG(curr_att->mime_type, no_fname);    
+        TEST_ASSERT_MSG(strcmp(curr_att->filename, 
+                               (*it).first.c_str()) == 0, 
+                        curr_att->filename);        
+        TEST_ASSERT_MSG(strcmp(curr_att->mime_type, 
+                                (*it).first.c_str()) == 0,
+                        curr_att->mime_type);        
+        it++;
+        curr_att = curr_att->next;
+    } 
+    
+    free_message(enc_msg);
+    free_message(dec_msg);
+    free_stringlist(keylist);
 }
