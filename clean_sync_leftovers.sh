fdf626df9568a6e9270adb6beaeb650ff2b776c0
Krista 'DarthMama' Bennett <krista@pep.foundation>
2019-04-09 09:05:49 +0200

    Added removal of untracked files for switch from sync to default

--- clean_sync_leftovers.sh
+++ clean_sync_leftovers.sh
@@ -4,3 +4,7 @@ cd src; hg status . | sed '/?\ /!d' | sed 's/?\ //' | xargs rm
 cd ../asn.1; hg status . | sed '/?\ /!d' | sed 's/?\ //' |  xargs rm
 cd ../sync; hg status . | sed '/?\ /!d' | sed 's/?\ //' |  xargs rm
 cd ..
+branch=`hg branch`
+if [ "$branch" = "sync" ]; then
+    rm src/KeySync_fsm.c src/KeySync_fsm.h src/Sync_actions.c src/Sync_event.c src/Sync_event.h src/Sync_func.c src/Sync_func.h src/Sync_impl.c src/Sync_impl.h src/sync_codec.c src/sync_codec.h
+fi	
