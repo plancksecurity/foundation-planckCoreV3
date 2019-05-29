34942916baa0f527a17aa3ecf240fbecc07b6424
Krista 'DarthMama' Bennett <krista@pep.foundation>
2019-05-29 19:33:45 +0200

    merge

--- test/src/SuiteMaker.cc
+++ test/src/SuiteMaker.cc
@@ -24,6 +24,7 @@
 #include "NoOwnIdentWritesOnDecryptTests.h"
 #include "LiteralFilenameTests.h"
 #include "I18nTests.h"
+#include "Message2_1Tests.h"
 #include "IdentityListTests.h"
 #include "PgpBinaryTests.h"
 #include "SubkeyRatingEvalTests.h"
@@ -88,6 +89,7 @@ const char* SuiteMaker::all_suites[] = {
     "NoOwnIdentWritesOnDecryptTests",
     "LiteralFilenameTests",
     "I18nTests",
+    "Message2_1Tests",
     "IdentityListTests",
     "PgpBinaryTests",
     "SubkeyRatingEvalTests",
@@ -140,7 +142,7 @@ const char* SuiteMaker::all_suites[] = {
 };
 
 // This file is generated, so magic constants are ok.
-int SuiteMaker::num_suites = 61;
+int SuiteMaker::num_suites = 62;
 
 void SuiteMaker::suitemaker_build(const char* test_class_name, const char* test_home, Test::Suite** test_suite) {
     if (strcmp(test_class_name, "URIAddressTests") == 0)
@@ -167,6 +169,8 @@ void SuiteMaker::suitemaker_build(const char* test_class_name, const char* test_
         *test_suite = new LiteralFilenameTests(test_class_name, test_home);
     else if (strcmp(test_class_name, "I18nTests") == 0)
         *test_suite = new I18nTests(test_class_name, test_home);
+    else if (strcmp(test_class_name, "Message2_1Tests") == 0)
+        *test_suite = new Message2_1Tests(test_class_name, test_home);
     else if (strcmp(test_class_name, "IdentityListTests") == 0)
         *test_suite = new IdentityListTests(test_class_name, test_home);
     else if (strcmp(test_class_name, "PgpBinaryTests") == 0)
