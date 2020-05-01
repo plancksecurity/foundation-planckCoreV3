eb694258bdc89357fca83a05ece5a4dbe9e42187
Volker Birk <vb@pep-project.org>
2020-05-01 10:49:08 +0200

    define internal message format

---
+++ src/internal_format.c
@@ -0,0 +1,8 @@
+// This file is under GNU General Public License 3.0
+// see LICENSE.txt
+
+#include "platform.h"
+
+#include "pEp_internal.h"
+#include "internal_format.h"
+
---
+++ src/internal_format.h
@@ -0,0 +1,14 @@
+// This file is under GNU General Public License 3.0
+// see LICENSE.txt
+
+#pragma once
+
+#include "message.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
