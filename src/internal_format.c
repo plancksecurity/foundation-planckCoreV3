753870de5f96bb90091a1e27e551b44dfbb43eec
Volker Birk <vb@pep-project.org>
2020-05-01 11:06:21 +0200

    function definitions

--- src/internal_format.c
+++ src/internal_format.c
@@ -6,3 +6,16 @@
 #include "pEp_internal.h"
 #include "internal_format.h"
 
+
+DYNAMIC_API PEP_STATUS encode_internal(const char *value, const char *mime_type, char **code)
+{
+
+    return PEP_STATUS_OK;
+}
+
+DYNAMIC_API PEP_STATUS decode_internal(const char *code, char **value, char **mime_type)
+{
+
+    return PEP_STATUS_OK;
+}
+
--- src/internal_format.h
+++ src/internal_format.h
@@ -9,6 +9,33 @@
 extern "C" {
 #endif
 
+// encode_internal() - encode to the internal message format
+//
+//  parameters:
+//      value (in)      blob
+//      mime_type (in)  string of MIME type
+//      code (out)      blob in Internal Message Format
+//
+//  see also:
+//      https://dev.pep.foundation/Engine/ElevatedAttachments
+
+DYNAMIC_API PEP_STATUS encode_internal(const char *value, const char *mime_type, char **code);
+
+
+// decode_internal() - decode from internal message format
+//
+//  parameters:
+//      code (in)       blob in Internal Message Format
+//      value (out)     blob or string for longmsg
+//      mime_type (out) string with MIME type or NULL for longmsg
+//
+//  caveat:
+//      in case there is no internal message in the code a string for longmsg
+//      is returned
+
+DYNAMIC_API PEP_STATUS decode_internal(const char *code, char **value, char **mime_type);
+
+
 #ifdef __cplusplus
 }
 #endif
