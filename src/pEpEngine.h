0bfb3f669e0852157486983a639a5f31dd7611c0
Krista 'DarthMama' Bennett <krista@pep.foundation>
2020-05-06 00:31:29 +0200

    Upgrading version constants - this is a losing battle with the current release process. But should be fixed soon.

--- src/pEpEngine.h
+++ src/pEpEngine.h
@@ -21,11 +21,11 @@ extern "C" {
 
 // RELEASE version this targets
 // (string: major.minor.patch)
-#define PEP_ENGINE_VERSION "2.0.0"
+#define PEP_ENGINE_VERSION "2.0.3"
 // Numeric values of above:
 #define PEP_ENGINE_VERSION_MAJOR 2
 #define PEP_ENGINE_VERSION_MINOR 0
-#define PEP_ENGINE_VERSION_PATCH 0
+#define PEP_ENGINE_VERSION_PATCH 3
 
 
 #define PEP_OWN_USERID "pEp_own_userId"
