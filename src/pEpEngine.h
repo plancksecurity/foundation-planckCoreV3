08cd375111428694678abb6dbafdbec8a9f99c26
Krista 'DarthMama' Bennett <krista@pep.foundation>
2020-09-08 10:52:07 +0200

    Automatically bumped RC in source for future release. Next RC after this one will be 2.1.0-RC31 **if released**.

--- src/pEpEngine.h
+++ src/pEpEngine.h
@@ -25,7 +25,7 @@ extern "C" {
 #define PEP_ENGINE_VERSION_MAJOR 2
 #define PEP_ENGINE_VERSION_MINOR 1
 #define PEP_ENGINE_VERSION_PATCH 0
-#define PEP_ENGINE_VERSION_RC    30
+#define PEP_ENGINE_VERSION_RC    31
 
 
 #define PEP_OWN_USERID "pEp_own_userId"
