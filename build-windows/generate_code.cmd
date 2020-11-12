2be54d0a1e627c66cd0c40ff537f9d50beca84b1
Krista 'DarthMama' Bennett <krista@pep.foundation>
2020-11-12 12:33:24 +0100

    Fixing graft mess ;)

--- build-windows/generate_code.cmd
+++ build-windows/generate_code.cmd
@@ -1,58 +1,52 @@
 @ECHO OFF
-PUSHD .
-SET pwd=%cd%
-CD %pwd%\sync
+
+:: The script is located in ...\pEpForWindowsAdapterSolution\pEpEngine\build-windows\
+SET current_directory=%~dp0
+
+:: Engine directory is ...\pEpForWindowsAdapterSolution\pEpEngine\
+SET engine_directory=%current_directory:~0,-14%
+
+:: YML2 directory is ...\pEpForWindowsAdapterSolution\yml2\
+SET yml2_directory=%engine_directory:~0,-11%\yml2
+
+:: Generate code in ...\pEpEngine\sync
+PUSHD %engine_directory%\sync
 
 IF NOT EXIST generated MKDIR generated
 
-ECHO py "%YML_PATH%\yml2proc" -E utf-8 -y gen_actions.ysl2 sync.fsm
-py "%YML_PATH%\yml2proc" -E utf-8 -y gen_actions.ysl2 sync.fsm
-IF %ERRORLEVEL% NEQ 0 (
-	POPD
-	EXIT /B 1
-	)
-ECHO py "%YML_PATH%\yml2proc" -E utf-8 -y gen_codec.ysl2 distribution.fsm
-py "%YML_PATH%\yml2proc" -E utf-8 -y gen_codec.ysl2 distribution.fsm
-IF %ERRORLEVEL% NEQ 0 (
-	POPD
-	EXIT /B 1
-	)
-ECHO py "%YML_PATH%\yml2proc" -E utf-8 -y gen_codec.ysl2 sync.fsm
-py "%YML_PATH%\yml2proc" -E utf-8 -y gen_codec.ysl2 sync.fsm
-IF %ERRORLEVEL% NEQ 0 (
-	POPD
-	EXIT /B 1
-	)
-ECHO py "%YML_PATH%\yml2proc" -E utf-8 -y gen_messages.ysl2 sync.fsm
-py "%YML_PATH%\yml2proc" -E utf-8 -y gen_messages.ysl2 sync.fsm
-IF %ERRORLEVEL% NEQ 0 (
-	POPD
-	EXIT /B 1
-	)
-ECHO py "%YML_PATH%\yml2proc" -E utf-8 -y gen_messages.ysl2 distribution.fsm
-py "%YML_PATH%\yml2proc" -E utf-8 -y gen_messages.ysl2 distribution.fsm
-IF %ERRORLEVEL% NEQ 0 (
-	POPD
-	EXIT /B 1
-	)
-ECHO py "%YML_PATH%\yml2proc" -E utf-8 -y gen_message_func.ysl2 sync.fsm
-py "%YML_PATH%\yml2proc" -E utf-8 -y gen_message_func.ysl2 sync.fsm
-IF %ERRORLEVEL% NEQ 0 (
-	POPD
-	EXIT /B 1
-	)
-ECHO py "%YML_PATH%\yml2proc" -E utf-8 -y gen_statemachine.ysl2 sync.fsm
-py "%YML_PATH%\yml2proc" -E utf-8 -y gen_statemachine.ysl2 sync.fsm
-IF %ERRORLEVEL% NEQ 0 (
-	POPD
-	EXIT /B 1
-	)
+ECHO py "%yml2_directory%\yml2proc" -E utf-8 -y gen_actions.ysl2 sync.fsm
+py "%yml2_directory%\yml2proc" -E utf-8 -y gen_actions.ysl2 sync.fsm
+IF %ERRORLEVEL% NEQ 0 GOTO end
 
-xcopy /y generated\*.asn1 ..\asn.1\
-xcopy /y generated\*.c ..\src\
-xcopy /y generated\*.h ..\src\
+ECHO py "%yml2_directory%\yml2proc" -E utf-8 -y gen_codec.ysl2 distribution.fsm
+py "%yml2_directory%\yml2proc" -E utf-8 -y gen_codec.ysl2 distribution.fsm
+IF %ERRORLEVEL% NEQ 0 GOTO end
 
-CD %pwd%\asn.1
+ECHO py "%yml2_directory%\yml2proc" -E utf-8 -y gen_codec.ysl2 sync.fsm
+py "%yml2_directory%\yml2proc" -E utf-8 -y gen_codec.ysl2 sync.fsm
+IF %ERRORLEVEL% NEQ 0 GOTO end
+
+ECHO py "%yml2_directory%\yml2proc" -E utf-8 -y gen_messages.ysl2 sync.fsm
+py "%yml2_directory%\yml2proc" -E utf-8 -y gen_messages.ysl2 sync.fsm
+IF %ERRORLEVEL% NEQ 0 GOTO end
+
+ECHO py "%yml2_directory%\yml2proc" -E utf-8 -y gen_messages.ysl2 distribution.fsm
+py "%yml2_directory%\yml2proc" -E utf-8 -y gen_messages.ysl2 distribution.fsm
+IF %ERRORLEVEL% NEQ 0 GOTO end
+
+ECHO py "%yml2_directory%\yml2proc" -E utf-8 -y gen_message_func.ysl2 sync.fsm
+py "%yml2_directory%\yml2proc" -E utf-8 -y gen_message_func.ysl2 sync.fsm
+IF %ERRORLEVEL% NEQ 0 GOTO end
+
+ECHO py "%yml2_directory%\yml2proc" -E utf-8 -y gen_statemachine.ysl2 sync.fsm
+py "%yml2_directory%\yml2proc" -E utf-8 -y gen_statemachine.ysl2 sync.fsm
+IF %ERRORLEVEL% NEQ 0 GOTO end
+
+XCOPY /y generated\*.asn1 ..\asn.1\
+XCOPY /y generated\*.c ..\src\
+XCOPY /y generated\*.h ..\src\
+
+CD %engine_directory%\asn.1
 
 DEL *.h
 DEL *.c
@@ -65,9 +59,13 @@ IF %ERRORLEVEL% NEQ 0 GOTO end
 
 DEL *-sample.c
 
-CD %pwd%\..
+CD %engine_directory%\..
 RD /S/Q pEp
 MKDIR pEp
 XCOPY pEpEngine\src\*.h pEp\ /Y/F/I
 
+
+:end
+
 POPD
+EXIT /B %ERRORLEVEL%
