4157dadc85bd49bea52311a06ed102957309eede
Krista 'DarthMama' Bennett <krista@pep.foundation>
2020-12-03 14:32:02 +0100

    ENGINE-822: fix for asn.1 constraint nonsense

--- src/pEp_internal.h
+++ src/pEp_internal.h
@@ -104,6 +104,9 @@
 #include "pgp_sequoia_internal.h"
 #endif
 
+#include "../asn.1/Distribution.h"
+#include "../asn.1/Sync.h"
+
 #include "keymanagement.h"
 #include "cryptotech.h"
 #include "transport.h"
@@ -730,9 +733,51 @@ static inline void set_max_version(unsigned int first_maj, unsigned int first_mi
 extern int _pEp_rand_max_bits;
 extern double _pEp_log2_36;
 
+
+
+/**
+ *  <!--         _fix_asn1_constraint()        -->
+ *
+ *  @internal
+ *
+ *  @brief      Re-initialise necessary asn.1 globals which are falsely set by the asn1c compiler for non-GCC
+ *
+ *  @see        Distribution.c:11
+ */
+static inline void _fix_asn1_constraint(asn_TYPE_descriptor_t* lang_def) {
+    struct asn_per_constraints_s* fixed_vals =
+            (struct asn_per_constraints_s*)malloc(sizeof(struct asn_per_constraints_s));
+
+    if (!memcpy(fixed_vals, lang_def->per_constraints, sizeof(struct asn_per_constraints_s)))
+        return;
+
+    fixed_vals->value.range_bits = 0;
+    fixed_vals->value.effective_bits = 0;
+    fixed_vals->value.lower_bound = 0;
+    fixed_vals->value.upper_bound = 0;
+
+    // We don't free the lang_def previous value because it was stack-allocated
+    lang_def->per_constraints = fixed_vals;
+}
+
+/**
+ *  <!--         _patch_asn1_codec()        -->
+ *
+ *  @internal
+ *
+ *  @brief      Re-initialise necessary asn.1 globals which are falsely set by the asn1c compiler
+ *
+ */
+static inline void _patch_asn1_codec() {
+    _fix_asn1_constraint(&asn_DEF_Distribution);
+    _fix_asn1_constraint(&asn_DEF_Sync);
+}
+
 /**
  *  <!--       _init_globals()       -->
- *  
+ *
+ *  @internal
+ *
  *  @brief            TODO
  *  
  *  
@@ -740,7 +785,9 @@ extern double _pEp_log2_36;
 static inline void _init_globals() {
     _pEp_rand_max_bits = (int) ceil(log2((double) RAND_MAX));
     _pEp_log2_36 = log2(36);
+    _patch_asn1_codec();
 }
+
 
 // spinlock implementation
 
