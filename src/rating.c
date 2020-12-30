024e499cfba8c9331d7851ac76e5d93d8adbb63c
Volker Birk <vb@pep-project.org>
2020-12-30 16:27:56 +0100

    rating will be a module

---
+++ src/rating.c
@@ -0,0 +1,10 @@
+/**
+ * @file    rating.c
+ * @brief   implementation of rating functions
+ * @license GNU General Public License 3.0 - see LICENSE.txt
+ */
+
+
+#include "pEp_internal.h"
+#include "rating.h"
+
---
+++ src/rating.h
@@ -0,0 +1,25 @@
+/**
+ * @file    rating.h
+ * @brief   rating functions
+ * @license GNU General Public License 3.0 - see LICENSE.txt
+ */
+
+#ifndef RATING_H
+#define RATING_H
+
+
+#include "message.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // RATING_H
+
