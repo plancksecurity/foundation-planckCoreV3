d54e9549c5a5a2eb4f20fa662361892908dcfc39
Devan Carpenter <dvn@pep-project.org>
2021-02-15 00:54:42 +0100

    CI: add basic gitlab-ci jobs
    Adds simple jobs for building the the Engine for Debian 10.
    
    Pre-built docker images containing Sequoia will be pulled down and used
    to build the Engine. Currently the Sequoia version is just the latest
    from the `pep-engine` branch. We should make this selectable in the near
    future.
    
    Upon successful compilation an image containing the build output will be
    pushed to an internal docker registry.

---
+++ scripts/debian10/build_pEpEngine_deps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+set -exo
+
+### YML2
+cd $BUILDROOT
+wget https://fdik.org/yml2.tar.bz2
+tar -xf yml2.tar.bz2
+rm yml2.tar.bz2
+
+
+### libetpan
+git clone https://github.com/fdik/libetpan $BUILDROOT/libetpan
+cd $BUILDROOT/libetpan
+test -f configure || NOCONFIGURE=absolutely ./autogen.sh
+./configure --prefix=${INSTPREFIX}/libetpan \
+    --without-openssl --without-gnutls --without-sasl \
+    --without-curl --without-expat --without-zlib \
+    --disable-dependency-tracking
+make -j$(nproc)
+make install
+echo "${libetpan_ver}">${INSTPREFIX}/libetpan.ver
+
+
+### ASN1c
+git clone https://github.com/vlm/asn1c.git $BUILDROOT/asn1c
+cd $BUILDROOT/asn1c
+git checkout tags/v0.9.28 -b pep-engine
+test -f configure || autoreconf -iv
+./configure --prefix=${INSTPREFIX}/asn1c
+make -j$(nproc) && make install
+echo "${asn1c_ver}">${INSTPREFIX}/asn1c.ver
