8766cd91de8a1b40e019a5919a84bad3c18e4e92
Krista Bennett <krista@pep.foundation>
2021-02-19 04:07:42 +0100

    I hate everything

--- scripts/project/git-filter-clean-pEpEngine-commit-hash.sh
+++ scripts/project/git-filter-clean-pEpEngine-commit-hash.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-echo "HELLO" >&2
 #
 # Clean filter for pEpEngine's commit-hash header.
 #
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    SEDFLAGS='' 
+    SED='sed -i '"'""'"
+else
+    SED="sed -i"
 fi
 
 echo "Replacing PEP_CURRENT_COMMIT_HASH value in src/commit_hash.h with DUMMY_COMMIT_HASH_ERROR. See you next checkout or after the commit!"
 
-$(sed -i $SEDFLAGS "s/\(PEP_CURRENT_COMMIT_HASH=\).*/\1\DUMMY_COMMIT_HASH_ERROR\"/" $1)
+$($SED "s/\(PEP_CURRENT_COMMIT_HASH=\).*/\1\DUMMY_COMMIT_HASH_ERROR\"/" $1)
 
