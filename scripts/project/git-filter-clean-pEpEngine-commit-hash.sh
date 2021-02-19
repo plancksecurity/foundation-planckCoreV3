be35443adb5a0645e82e7f864e0ab11c118fb076
Krista Bennett <krista@pep.foundation>
2021-02-19 02:00:15 +0100

    Commiting smudge and clean filters for testing. If they work, all folks compiling the engine will need to execute some config commands or modify their .git/config. More to come.

---
+++ scripts/project/git-filter-clean-pEpEngine-commit-hash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# Clean filter for pEpEngine's commit-hash header.
+#
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED="sed -i ''"
+else
+    SED="sed -i"
+fi
+
+echo "Replacing PEP_CURRENT_COMMIT_HASH value in src/commit_hash.h with DUMMY_COMMIT_HASH_ERROR. See you next checkout or after the commit!"
+
+$($SED "s/\(PEP_CURRENT_COMMIT_HASH=\).*/\1\DUMMY_COMMIT_HASH_ERROR\"/" $1)
+
---
+++ scripts/project/git-filter-smudge-pEpEngine-commit-hash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Smudge file for pEpEngine's commit-hash header
+#
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED="sed -i ''"
+else
+    SED="sed -i"
+fi
+
+commit_hash="$(git rev-parse HEAD)"
+echo "Replacing PEP_CURRENT_COMMIT_HASH value in src/commit_hash.h with current HEAD commit hash, $commit_hash. Will clean on commit or checkout."
+
+$($SED "s/\(PEP_CURRENT_COMMIT_HASH=\).*/\1\"$commit_hash\"/" $1)
+
