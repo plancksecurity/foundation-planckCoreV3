444fcd0087340e74b44a48f9ffa548e8d4c2deaa
Krista Bennett <krista@pep.foundation>
2021-02-19 04:13:08 +0100

    MacOS and git bash do STUPID things together with sed.

--- scripts/project/git-filter-clean-pEpEngine-commit-hash.sh
+++ scripts/project/git-filter-clean-pEpEngine-commit-hash.sh
@@ -14,3 +14,7 @@ echo "Replacing PEP_CURRENT_COMMIT_HASH value in src/commit_hash.h with DUMMY_CO
 
 $($SED "s/\(PEP_CURRENT_COMMIT_HASH=\).*/\1\DUMMY_COMMIT_HASH_ERROR\"/" $1)
 
+# Honestly, I have no idea what git is doing with the stupid empty '' for sed, but I give up. It makes a commit_hash.h'' backup for no reason. So we eat it.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    rm $1"'""'"
+else
