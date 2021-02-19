fa3ee20723a46e226cc9936f745addc4ba99abfb
Krista Bennett <krista@pep.foundation>
2021-02-19 03:33:21 +0100

    SED, take me away...

--- scripts/project/git-filter-clean-pEpEngine-commit-hash.sh
+++ scripts/project/git-filter-clean-pEpEngine-commit-hash.sh
@@ -5,11 +5,11 @@ echo "HELLO" >&2
 # Clean filter for pEpEngine's commit-hash header.
 #
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED="sed -i ''"
-else
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#    SED="sed -i ''"
+#else
     SED="sed -i"
-fi
+#fi
 
 echo "Replacing PEP_CURRENT_COMMIT_HASH value in src/commit_hash.h with DUMMY_COMMIT_HASH_ERROR. See you next checkout or after the commit!"
 
