a6fb33c8c00b834c663c56caf7dc8518395c67f7
Krista Bennett <krista@pep.foundation>
2021-02-22 11:40:59 +0100

    ENGINE-883: files for commit hash work. Still need to set up the how-to and ensure we go back and forth to older commits without choking.

--- scripts/git/git-filter-clean-pEpEngine-commit-hash.sh
+++ scripts/git/git-filter-clean-pEpEngine-commit-hash.sh
@@ -10,7 +10,6 @@ else
     SED="sed -i"
 fi
 
-echo "FILE is $1"
 echo "Replacing PEP_CURRENT_COMMIT_HASH value in src/commit_hash.h with DUMMY_COMMIT_HASH_ERROR. See you next checkout or after the commit!"
 
 $($SED "s/\(PEP_CURRENT_COMMIT_HASH=\).*/\1\DUMMY_COMMIT_HASH_ERROR\"/" $1)
@@ -18,4 +17,4 @@ $($SED "s/\(PEP_CURRENT_COMMIT_HASH=\).*/\1\DUMMY_COMMIT_HASH_ERROR\"/" $1)
 # Honestly, I have no idea what git is doing with the stupid empty '' for sed, but I give up. It makes a commit_hash.h'' backup for no reason. So we eat it.
 if [[ "$OSTYPE" == "darwin"* ]]; then
     rm $1"'""'"
-else
+fi
