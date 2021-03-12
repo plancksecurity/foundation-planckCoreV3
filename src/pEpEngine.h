6b93955f5be0c9c8440b71a5d9754e7e9468bb1f
Krista Bennett <krista@pep.foundation>
2021-03-12 16:21:53 +0100

    bumped patch for next release

--- src/pEpEngine.h
+++ src/pEpEngine.h
@@ -22,10 +22,10 @@ extern "C" {
 
 // RELEASE version this targets
 // (string: major.minor.patch)
-#define PEP_ENGINE_VERSION "2.1.18"
+#define PEP_ENGINE_VERSION "2.1.19"
 #define PEP_ENGINE_VERSION_MAJOR 2
 #define PEP_ENGINE_VERSION_MINOR 1
-#define PEP_ENGINE_VERSION_PATCH 18 
+#define PEP_ENGINE_VERSION_PATCH 19 
 #define PEP_ENGINE_VERSION_RC    0
 
 
