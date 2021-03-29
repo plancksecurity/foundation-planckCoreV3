736aa6c95eb34f15540774e8348211180b1922a2
Andreas Buff <andreas@pep-security.net>
2021-03-29 17:13:13 +0200

    merge IOS-2742

---
+++ build-mac/Subprojects/PEPSQLITE3/PEPSQLITE3_macOS/PEPSQLITE3_macOS.h
@@ -0,0 +1,12 @@
+//
+//  PEPSQLITE3_macOS.h
+//  PEPSQLITE3_macOS
+//
+//  Created by Andreas Buff on 29.03.21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PEPSQLITE3_macOS : NSObject
+
+@end
