dd7df47f526ad1166116735ef119c64e470a7ad8
Andreas Buff <andreas@pep-security.net>
2021-03-29 13:20:48 +0200

    merge IOS-2740

---
+++ build-mac/pEpEngine_macOS/pEpEngine_macOS.m
@@ -0,0 +1,13 @@
+//
+//  pEpEngine_macOS.m
+//  pEpEngine_macOS
+//
+//  Created by Andreas Buff on 02.12.20.
+//  Copyright © 2020 p≡p Security S.A. All rights reserved.
+//
+
+#import "pEpEngine_macOS.h"
+
+@implementation pEpEngine_macOS
+
+@end
