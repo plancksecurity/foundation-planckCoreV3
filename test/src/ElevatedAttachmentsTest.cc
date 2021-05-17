fc0376f3ecde1832ef4cb90067373108efce572c
Krista Bennett <krista@pep.foundation>
2021-05-17 19:12:13 +0200

    ENGINE-633: ElevatedAttachmentsTest update_identity assumption fixed

--- test/src/ElevatedAttachmentsTest.cc
+++ test/src/ElevatedAttachmentsTest.cc
@@ -312,7 +312,11 @@ TEST_F(ElevatedAttachmentsTest, check_encrypt_decrypt_message_elevated) {
     ASSERT_OK;
     status = update_identity(session, bob);
     ASSERT_OK;
-
+    bob->fpr = strdup(bob_fpr);
+    status = set_identity(session, bob);
+    ASSERT_OK;
+    status = update_identity(session, bob);
+    ASSERT_OK;
     status = set_as_pEp_user(session, bob);
     ASSERT_OK;
 
