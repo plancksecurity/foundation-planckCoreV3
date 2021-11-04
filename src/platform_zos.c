19a4d2eda4ae294d53429f00d7a19b364c0d7b06
Luca Saiu <positron@pep.foundation>
2021-11-04 13:44:10 +0100

    move some z/OS definitions to a platform file: very tentative
    Not yet really tested on z/OS, so I expect things to break there.  But Unix
    should keep working.  Waiting for feedback from Lev.

---
+++ src/platform_zos.c
@@ -0,0 +1,15 @@
+// This file is under GNU General Public License 3.0
+// see LICENSE.txt
+
+/* Include the entire useful part of this compilation unit within a CPP
+   conditional, so that the entire file expands to nothing if this is not
+   the platform it is used on. */
+#ifdef ZOS
+
+#include "pEpEngine.h"
+#include "platform_zos.h"
+#include "dynamic_api.h"
+
+/* Nothing useful here at this time. */
+
+#endif // #ifdef ZOS
