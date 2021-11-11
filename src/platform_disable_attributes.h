410fcc41da058a09cdaa33e81bd65245f6742c4e
Luca Saiu <positron@pep.foundation>
2021-11-11 10:57:19 +0100

    comment change

--- src/platform_disable_attributes.h
+++ src/platform_disable_attributes.h
@@ -1,10 +1,10 @@
 #ifndef PLATFORM_DISABLE_ATTRIBUTES_H
 #define PLATFORM_DISABLE_ATTRIBUTES_H
 
-/* Disable GNU-style attributes
+/* Disable GNU-style attributes for functions, variables, types and label
    (https://gcc.gnu.org/onlinedocs/gcc/Function-Attributes.html ), turning them
    into noops.  This lets the code compile with compilers not supporting
-   attributes, while keeping them enabled when they matter.
+   attributes, while keeping them enabled where they matter.
 
    This header is included by platform-specific code where needed: it is not
    for the user to directly see. */
