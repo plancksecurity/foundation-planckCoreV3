835959283ec406ced4590e02248a762c9a18335b
Luca Saiu <positron@pep.foundation>
2022-01-13 16:03:09 +0100

    TKA: add stub C file

---
+++ src/tka_api.c
@@ -0,0 +1,11 @@
+/**
+ * @file     tka_api.c
+ * @brief    implementation of TempKey-Agreement API
+ * @license  GNU General Public License 3.0 - see LICENSE.txt
+ */
+
+#include "pEp_internal.h"
+#include "tka_api.h"
+#include "pEpEngine.h"
+
+#include "platform.h"
