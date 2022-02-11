7ccf55cb6ec3a2275e8e3ca51ebdf3a1be3efdaf
Luca Saiu <positron@pep.foundation>
2022-02-11 13:11:32 +0100

    after RC, re-add "+" to version

--- src/pEpEngine.h
+++ src/pEpEngine.h
@@ -36,7 +36,7 @@ extern "C" {
                                      // which must always be defined
 #define PEP_ENGINE_VERSION_RC    3   // the last already-released RC for this
                                      // patchlevel, or not defined if not an RC
-//#define PEP_ENGINE_VERSION_PLUS  +   // Only defined for tagged versions
+#define PEP_ENGINE_VERSION_PLUS  +   // Only defined for tagged versions
 
 /* The following macros serve to machine-generate a definition of
    PEP_ENGINE_VERSION, following the correct format as a string, from the
