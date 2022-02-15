d009122ad3689bd9f1163f7f29753cfc20713005
Luca Saiu <positron@pep.foundation>
2022-02-15 11:47:13 +0100

    change version for RC

--- src/pEpEngine.h
+++ src/pEpEngine.h
@@ -34,9 +34,9 @@ extern "C" {
 #define PEP_ENGINE_VERSION_PATCH 51  // the last already-released or
                                      // being-released patchlevel,
                                      // which must always be defined
-#define PEP_ENGINE_VERSION_RC    3   // the last already-released RC for this
+#define PEP_ENGINE_VERSION_RC    4   // the last already-released RC for this
                                      // patchlevel, or not defined if not an RC
-#define PEP_ENGINE_VERSION_PLUS  +   // Only defined for tagged versions
+  //#define PEP_ENGINE_VERSION_PLUS  +   // Only defined for tagged versions
 
 /* The following macros serve to machine-generate a definition of
    PEP_ENGINE_VERSION, following the correct format as a string, from the
