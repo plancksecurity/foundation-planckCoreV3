e02f1085f7d41ccb9331628c099cde20484fc83b
Dirk Zimmermann <dz@pep.security>
2022-03-21 16:00:05 +0100

    IOS-2939 Take over changes from the original iOS version

--- build-mac/scripts/python/update_engine_generated_files.py
+++ build-mac/scripts/python/update_engine_generated_files.py
@@ -65,20 +65,18 @@ clean_engine()
 
 asn_files_before_set = set(cfiles(ASN_DIR_GENERATED))
 
-generate_files()
+generate_sync_files()
 
-asn_files_after_set = set(sorted(cfiles(ASN_DIR_GENERATED)))
-asn_diff = asn_files_after_set.difference(asn_files_before_set)
-asn_files = sorted(asn_files_after_set)
+asn_files_after = sorted(cfiles(ASN_DIR_GENERATED))
+sync_files = sorted(cfiles(SYNC_DIR_GENERATED))
 
 with open(DEST_ASN_LIST, 'w') as asn_target_file:
-    for asn_file in asn_files:
-        item = '$(SRCROOT)/../../../asn.1/' + asn_file
-        asn_target_file.write(item + '\n')
-
-sync_files_after = sorted(cfiles(SYNC_DIR_GENERATED))
+    for asn_file in asn_files_after:
+        if not (asn_file in asn_files_before_set):
+            item = '$(SRCROOT)/../asn.1/' + asn_file
+            asn_target_file.write(item + '\n')
 
 with open(DEST_SYNC_LIST, 'w') as sync_target_file:
-    for sync_file in sync_files_after:
+    for sync_file in sync_files:
         item = '$(SRCROOT)/../src/' + sync_file
         sync_target_file.write(item + '\n')
