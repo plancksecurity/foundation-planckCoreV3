9c597b66c345078eb8b1924dad763fc13340fd61
Gernot Kranz <gernot@pep.foundation>
2022-03-21 15:04:41 +0100

    msc playground

---
+++ src/playground.h
@@ -0,0 +1,12 @@
+/** @file playground.h */
+/**
+ * @struct Iberian_Dance_Task
+ * @brief something evil 
+ * @msc
+ *  T,"Iberian Dance Task";
+ *
+ *  T->"Iberian Dance Task" [label="IbFandangoReq", URL="\ref IbFandangoReq"];
+ *  T<<"Iberian Dance Task" [label="IbFandangoCnf", URL="\ref IbFandangoCnf", ID="1"];
+ *  @endmsc
+ */
+struct Iberian_Dance_Task;
