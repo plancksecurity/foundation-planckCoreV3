e3086ebdcbdd11284180d65d0a6924753569a6d3
Luca Saiu <positron@pep.foundation>
2022-09-15 18:31:19 +0200

    media key: fix bug in config_media_keys reported by dirk
    config_media_keys: Before adding the new entries to the map, make the map null.
    The code is slightly complicated by the need to keep the *old* map alive in
    order to handle the out-of-memory case cleanly, but this was still a very ugly
    bug.  Thanks Dirk for reporting it.
    
    Thanks dirk for reporting.

--- src/media_key.c
+++ src/media_key.c
@@ -176,7 +176,8 @@ DYNAMIC_API PEP_STATUS config_media_keys(PEP_SESSION session,
     /* Keep the old map, to be restored in case of any error. */
     stringpair_list_t *old_map = session->media_key_map;
 
-    /* Do the work: add every pair. */
+    /* Do the work: add every pair to the current map. */
+    session->media_key_map = NULL;
     PEP_STATUS status = PEP_STATUS_OK;
     for (rest = new_map; rest != NULL; rest = rest->next) {
             const stringpair_t *pair = rest->value;
