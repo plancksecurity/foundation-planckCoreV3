439e8e9dfd4cfa97b9fe79c732916d5d600ecf64
Luca Saiu <positron@pep.foundation>
2022-10-11 15:27:31 +0200

    WIP debug: add C stubs and build system support

---
+++ src/debug.h
@@ -0,0 +1,39 @@
+/**
+ * @file    debug.h
+ * @brief   pEp Engine debugging facilities
+ * @license GNU General Public License 3.0 - see LICENSE.txt
+ */
+
+// (setq show-trailing-whitespace t indicate-empty-lines t)
+
+#ifndef PEP_ENGINE_DEBUG_H
+#define PEP_ENGINE_DEBUG_H
+
+#include "pEpEngine.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Introduction
+ * ***************************************************************** */
+
+//...
+
+
+/* Modes
+ * ***************************************************************** */
+
+
+/* Requirements
+ * ***************************************************************** */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif // #ifndef PEP_ENGINE_DEBUG_H
