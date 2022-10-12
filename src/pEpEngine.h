fc7cdbeee098e2b0263e4943b14300b0cb7d1090
Luca Saiu <positron@pep.foundation>
2022-10-12 18:28:41 +0200

    release: add + suffix

--- src/pEpEngine.h
+++ src/pEpEngine.h
@@ -45,7 +45,7 @@ extern "C" {
                                      // which must always be defined
 #define PEP_ENGINE_VERSION_RC    9   // the last already-released RC for this
                                      // patchlevel, or not defined if not an RC
-//#define PEP_ENGINE_VERSION_PLUS  +   // Only defined when the repo is in a
+#define PEP_ENGINE_VERSION_PLUS  +   // Only defined when the repo is in a
                                      // non-released non-RC state.
 
 /* The following macros serve to machine-generate a definition of
