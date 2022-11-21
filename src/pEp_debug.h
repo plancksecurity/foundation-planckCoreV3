e0cfeff53b770283a41bac448a18e8887074545c
Luca Saiu <positron@pep.foundation>
2022-11-21 12:34:25 +0100

    whitespace

--- src/pEp_debug.h
+++ src/pEp_debug.h
@@ -97,7 +97,7 @@ typedef enum {
    which is always included anyway, and the logging level which is
    "function". */
 #if defined (PEP_LOG_FUNCTION_ENTRY)
-#   define _PEP_LOG_FUNCTION_ENTRY_IF_ENABLED                                 \
+#   define _PEP_LOG_FUNCTION_ENTRY_IF_ENABLED  \
         PEP_LOG_FUNCTION("p≡p", "Engine", "")
 #else
 #   define _PEP_LOG_FUNCTION_ENTRY_IF_ENABLED  \
