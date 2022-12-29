7aed344e487ad71d97c0cbae33ecac0711293e91
nkls <nkls@pep.foundation>
2022-12-29 15:29:44 +0100

    test

---
+++ src/mixnet.c
@@ -0,0 +1,29 @@
+/**
+  * @file	mixnet.c
+  * @brief	implementation of mixnet functionality for the pEp Engine
+  * @license	GNU General Public License 3.0 - see LICENSE.txt
+  */
+
+#include "pEp_internal.h"
+#include "mixnet.h"
+
+DYNAMIC_API PEP_STATUS onionize_message(
+	PEP_SESSION session,
+	message *src,
+	message **dst
+	)
+	{
+	PEP_STATUS status = PEP_STATUS_OK;
+
+	if(!(session && src && src->from && dst))
+		return PEP_ILLEGAL_VALUE;
+
+	*dst=NULL;
+
+enomem:
+	status = PEP_OUT_OF_MEMORY;
+
+pEp_error:
+	return status;
+
+	}
---
+++ src/mixnet.h
@@ -0,0 +1,44 @@
+/**
+  * @file	mixnet.h
+  * @brief	module for mixnet functions in the pEp Engine
+  * @license	GNU General Public License 3.0 - see LICENSE.txt
+  */
+
+#ifndef MIXNET_H
+#define MIXNET_H
+
+#include "pEpEngine.h"
+#include "message_api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+  * <!--	onionize_message()	-->
+  *
+  * @brief Onionionize Message; encrypt it several times
+  *
+  * @param[in]		session		session handle
+  * @param[in]		src		message to onionize
+  * @param[out]		dst		pointer to the onionized message
+  *					or NULL if no encryption could take place
+  * @retval PEP_STATUS_OK		on success
+  * @retval PEP_ILLEGAL_VALUE		illegal parameter values
+  * @retval PEP_OUT_OF_MEMORY		out of memory
+  *
+  * @warning		need more clarity about enc_format and flags
+  */
+
+DYNAMIC_API PEP_STATUS onionize_message
+	(
+	PEP_SESSION session,
+	message *src,
+	message **dst
+	);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
