52b26616047c997e3a1ed06519ed2746e264d3fe
Luca Saiu <positron@pep.foundation>
2023-02-20 16:25:25 +0100

    set version to 3.2.0-RC24

--- src/pEpEngine_version.h
+++ src/pEpEngine_version.h
@@ -12,13 +12,13 @@
 #define PEP_ENGINE_VERSION_MINOR          2
 #define PEP_ENGINE_VERSION_PATCH          0
 
-#define PEP_ENGINE_VERSION_RC             23  /* This is a Release Candidate. */
+#define PEP_ENGINE_VERSION_RC             24  /* This is a Release Candidate. */
 
-#define PEP_ENGINE_VERSION_PLUS           +   /* This is a MODIFIED Release Candidate */
+/* PEP_ENGINE_VERSION_PLUS not defined: this is an UNmodified Release Candidate. */
 
 /* PEP_ENGINE_VERSION_BRANCH_SUFFIX not defined. */
 
 /* The main version string for the user. */
-#define PEP_ENGINE_VERSION "3.2.0-RC23+"
+#define PEP_ENGINE_VERSION "3.2.0-RC24"
 
 #endif /* #ifndef _PEP_ENGINE_VERSION_H_ */
