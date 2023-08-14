c0301603436ad6d706b997dbdf8b7a1a66ba2b6b
ip <ip@planck.security>
2023-08-14 17:01:58 +0200

    bumped version to 3.3.1

--- src/pEpEngine_version.h
+++ src/pEpEngine_version.h
@@ -5,20 +5,23 @@
  *            --next-minor-rc --release
  * @license   GNU General Public License 3.0 - see LICENSE.txt
  */
+
+// 14.08.2023/IP - bumped version to 3.3.1
+
 #ifndef _PEP_ENGINE_VERSION_H_
 #define _PEP_ENGINE_VERSION_H_
 
 #define PEP_ENGINE_VERSION_MAJOR          3
 #define PEP_ENGINE_VERSION_MINOR          3
-#define PEP_ENGINE_VERSION_PATCH          0
+#define PEP_ENGINE_VERSION_PATCH          1
 
-#define PEP_ENGINE_VERSION_RC             4   /* This is a Release Candidate. */
+// #define PEP_ENGINE_VERSION_RC             4   /* This is a Release Candidate. */
 
 /* PEP_ENGINE_VERSION_PLUS not defined: this is an UNmodified Release Candidate. */
 
 /* PEP_ENGINE_VERSION_BRANCH_SUFFIX not defined. */
 
 /* The main version string for the user. */
-#define PEP_ENGINE_VERSION "3.3.0-RC4"
+#define PEP_ENGINE_VERSION "3.3.1"
 
 #endif /* #ifndef _PEP_ENGINE_VERSION_H_ */
