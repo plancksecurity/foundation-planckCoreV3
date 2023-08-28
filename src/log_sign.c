11f1ea0cc72b84ba4dc7fbcc8b72c3dd4accf61a
Dirk Zimmermann <dz@planck.security>
2023-08-28 11:41:27 +0200

    CORE-98 Add log_sign skeleton

---
+++ src/log_sign.c
@@ -0,0 +1,4 @@
+/**
+ * @file    log_sign.c
+ * @license GNU General Public License 3.0 - see LICENSE.txt
+ */
---
+++ src/log_sign.h
@@ -0,0 +1,17 @@
+/**
+ * @file    log_sign.h
+ * @license GNU General Public License 3.0 - see LICENSE.txt
+ */
+
+#ifndef LOG_SIGN_H
+#define LOG_SIGN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
