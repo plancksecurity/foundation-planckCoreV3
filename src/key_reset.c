f14c94ea4232ad97abdda0fbf2afb2cfd441f889
Dirk Zimmermann <dz@planck.security>
2023-10-24 15:43:22 +0200

    CORE-191 Comment

--- src/key_reset.c
+++ src/key_reset.c
@@ -1296,6 +1296,8 @@ static PEP_STATUS _do_full_reset_on_single_own_ungrouped_identity(PEP_SESSION se
     // we get into the current function.
     config_passphrase(session, session->generation_passphrase);
 
+    // Install the new key as own    
+
     free(ident->fpr);
     ident->fpr = NULL;
     status = set_own_key(session, ident, new_key);
