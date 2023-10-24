f59bcfc74a98879723ca4483c5b7339ff3f28b8f
Dirk Zimmermann <dz@planck.security>
2023-10-24 15:22:22 +0200

    CORE-191 Separate concerns

--- src/key_reset.c
+++ src/key_reset.c
@@ -1263,6 +1263,7 @@ static PEP_STATUS _do_full_reset_on_single_own_ungrouped_identity(PEP_SESSION se
             status = send_key_reset_to_active_group_members(session, ident, manager, old_fpr, new_key);
         }
     }
+
     if (status == PEP_STATUS_OK) {
         status = send_key_reset_to_recents(session, ident, old_fpr, new_key);
     }
