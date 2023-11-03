52c96922bf7292e6db2142e585f3b8cbb5c21a0e
Dirk Zimmermann <dz@planck.security>
2023-11-03 14:31:25 +0100

    Version 3.3.11

--- src/pEpEngine_version.h
+++ src/pEpEngine_version.h
@@ -20,7 +20,7 @@
 
 #define PEP_ENGINE_VERSION_MAJOR          3
 #define PEP_ENGINE_VERSION_MINOR          3
-#define PEP_ENGINE_VERSION_PATCH          9
+#define PEP_ENGINE_VERSION_PATCH          11
 
 // #define PEP_ENGINE_VERSION_RC             4   /* This is a Release Candidate. */
 
@@ -29,6 +29,6 @@
 /* PEP_ENGINE_VERSION_BRANCH_SUFFIX not defined. */
 
 /* The main version string for the user. */
-#define PEP_ENGINE_VERSION "3.3.9"
+#define PEP_ENGINE_VERSION "3.3.11"
 
 #endif /* #ifndef _PEP_ENGINE_VERSION_H_ */
