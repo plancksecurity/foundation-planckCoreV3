b708fd8aa723ed54d68be1a64866e53a9a31676a
Dirk Zimmermann <dz@planck.security>
2023-11-14 11:37:22 +0100

    CORE-213 Group key reset only to group members

--- src/key_reset.c
+++ src/key_reset.c
@@ -1273,10 +1273,7 @@ static PEP_STATUS _do_full_reset_on_single_own_ungrouped_identity(PEP_SESSION se
         if (status == PEP_STATUS_OK) {
             status = send_key_reset_to_active_group_members(session, local_ident, manager, old_fpr, new_key);
         }
-    }
-
-    // Note: On PEP_SYNC_NO_MESSAGE_SEND_CALLBACK, there is no point to try sending in any case.
-    if (status == PEP_STATUS_OK) {
+    } else {
         status = send_key_reset_to_recents(session, local_ident, old_fpr, new_key);
     }
 
