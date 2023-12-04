3dd9a01bf5438ae8fe9e32621d8ef592322b772f
ignaciogarcia <ig@pep.security>
2023-12-04 21:58:40 +0100

    CORE-230 update GPGBUILD name
    GPG stands for Gnu Privacy Guard, that variable is the build path, ie the outputh.
    Additionally many useless lines and icludes have been removed.

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -10,10 +10,6 @@ $(warning ==== IN PEPENGINE: UUID BUILT MODULE: $(MY_UUID_BUILD))
 
 include $(CLEAR_VARS)
 
-ifeq ($(GPGBUILD),)
-$(error GPGBUILD must be set)
-endif
-
 LOCAL_MODULE    := pEpEngine
 LOCAL_CFLAGS    += -std=c99 $(PEP_LOG)
 
@@ -28,22 +24,19 @@ LOCAL_CFLAGS    += -DSQLITE_TEMP_STORE=3 -DUSE_SEQUOIA
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../asn.1 \
 
-LOCAL_C_INCLUDES += $(GPGBUILD)/$(TARGET_ARCH_ABI)/include
+LOCAL_C_INCLUDES += $(BUILD_PATH)/$(TARGET_ARCH_ABI)/include
 LOCAL_C_INCLUDES += $(LIB_PEP_TRANSPORT_PATH)/build-android/include/
-#LOCAL_EXPORT_C_INCLUDES += $(LIB_PEP_TRANSPORT_PATH)/build-android/include/
-#LOCAL_EXPORT_C_INCLUDES += $(GPGBUILD)/$(TARGET_ARCH_ABI)/include
-#LOCAL_EXPORT_C_INCLUDES += $(GPGBUILD)/$(TARGET_ARCH_ABI)/include
+
 LOCAL_C_INCLUDES += $(LIB_PEP_TRANSPORT_PATH)/src
-#LOCAL_EXPORT_C_INCLUDES += $(LIB_PEP_TRANSPORT_PATH)/src
+
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
-#LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../include
 
 $(shell sh $(LOCAL_PATH)/../takeOutHeaderFiles.sh $(LOCAL_PATH)../../)
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)../include
 
-#ENGINE_SRC_FILES := $(shell find $(LOCAL_PATH)/../../src/ ! -name "*sequoia*" ! -name "*netpgp*" -name "*.c")
+#Exclude GPP and netpgp files, we could also exclude sequoia but that will never happen
 ENGINE_SRC_FILES := $(shell find $(LOCAL_PATH)/../../src/ ! -name "*gpg*" ! -name "*netpgp*" -name "*.c")
-#ENGINE_SRC_FILES := $(wildcard $(LOCAL_PATH)/../../src/*.c)
+
 ASN1_SRC_FILES := $(wildcard $(LOCAL_PATH)/../../asn.1/*.c)
 LOCAL_SRC_FILES := $(ENGINE_SRC_FILES:%=%)  $(ASN1_SRC_FILES:$(LOCAL_PATH)/%=%)
 
