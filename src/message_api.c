b26071e3d2b74fe41cd3210f1c7f03263b09cf45
Dirk Zimmermann <dz@planck.security>
2024-03-06 15:46:49 +0100

    CORE-346 Also set the user_id

--- src/message_api.c
+++ src/message_api.c
@@ -5259,6 +5259,9 @@ static void mark_identity_as_own(
         free(identity->fpr); // Could have been an allocated emtpy string
         identity->fpr = strdup(own_identity->fpr);
     }
+    if (EMPTYSTR(identity->user_id) && !EMPTYSTR(own_identity->user_id)) {
+        identity->user_id = strdup(own_identity->user_id);
+    }
 }
 
 static void fix_own_identity(
