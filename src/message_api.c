ed910f8793c1572d0a1e090db0bd7fa3a4cef7d6
Dirk Zimmermann <dz@planck.security>
2024-04-17 15:13:33 +0200

    CORE-261 Free this list when not needed

--- src/message_api.c
+++ src/message_api.c
@@ -4317,8 +4317,11 @@ static PEP_STATUS import_keys_from_decrypted_msg(PEP_SESSION session,
             il->ident->me = true;
         }
         free(own_id);
-        if (!status)
+        if (!status) {
             *private_il = _private_il;
+        } else {
+            free_identity_list(_private_il);
+        }
     }
     else {
         free_identity_list(_private_il);
