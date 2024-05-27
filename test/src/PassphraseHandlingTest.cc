9bff679cd060a8551f1b1ecade216506a7136112
Dirk Zimmermann <dz@planck.security>
2024-05-27 12:22:23 +0200

    CORE-434 Test has_passphrase

---
+++ test/src/PassphraseHandlingTest.cc
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <string>
+#include <cstring>
+
+#include "pEpEngine.h"
+#include "pEp_internal.h"
+#include "TestUtilities.h"
+#include "TestConstants.h"
+#include "Engine.h"
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+namespace {
+
+	// Tests for RFC-16 Passphrase Handling
+    class PassphraseHandlingTest : public ::testing::Test {
+        public:
+            Engine *engine;
+            PEP_SESSION session;
+
+        protected:
+            // You can remove any or all of the following functions if its body
+            // is empty.
+            PassphraseHandlingTest() {
+                // You can do set-up work for each test here.
+                test_suite_name = ::testing::UnitTest::GetInstance()->current_test_info()->GTEST_SUITE_SYM();
+                test_name = ::testing::UnitTest::GetInstance()->current_test_info()->name();
+                test_path = get_main_test_home_dir() + "/" + test_suite_name + "/" + test_name;
+            }
+
+            ~PassphraseHandlingTest() override {
+                // You can do clean-up work that doesn't throw exceptions here.
+            }
+
+            // If the constructor and destructor are not enough for setting up
+            // and cleaning up each test, you can define the following methods:
+
+            void SetUp() override {
+                // Code here will be called immediately after the constructor (right
+                // before each test).
+
+                // Leave this empty if there are no files to copy to the home directory path
+                std::vector<std::pair<std::string, std::string>> init_files = std::vector<std::pair<std::string, std::string>>();
+
+                // Get a new test Engine.
+                engine = new Engine(test_path);
+                ASSERT_NOTNULL(engine);
+
+                // Ok, let's initialize test directories etc.
+                engine->prep(NULL, NULL, NULL, init_files);
+
+                // Ok, try to start this bugger.
+                engine->start();
+                ASSERT_NOTNULL(engine->session);
+                session = engine->session;
+
+                // Engine is up. Keep on truckin'
+            }
+
+            void TearDown() override {
+                // Code here will be called immediately after each test (right
+                // before the destructor).
+                engine->shut_down();
+                delete engine;
+                engine = NULL;
+                session = NULL;
+            }
+            
+            const char *tyrell_email = "tyrell@example.com";
+
+            const char *tyrell_no_passphrase_fpr = "C203044D09E2EC0BD56FD32B66C9C6A984B31398";
+            const char *tyrell_no_passphrase_filename = "test_keys/passphrase_handling/tyrell_no_passphrase.pgp";
+            
+        private:
+            const char* test_suite_name;
+            const char* test_name;
+            string test_path;
+            // Objects declared here can be used by all tests in the PassphraseHandlingTest suite.
+
+    };
+
+}  // namespace
+
+TEST_F(PassphraseHandlingTest, tyrell_no_passphrase) {
+    ASSERT_TRUE(slurp_and_import_key(session, tyrell_no_passphrase_filename));
+    bool passphrase_bool = false;
+    PEP_STATUS status = has_passphrase(tyrell_email, &passphrase_bool);
+    ASSERT_EQ(status, PEP_STATUS_OK);
+    ASSERT_FALSE(passphrase_bool);
+}
