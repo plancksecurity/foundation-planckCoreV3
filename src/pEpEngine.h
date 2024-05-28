e80571efd09f52b772e092f56a95bf00a8f174a0
Dirk Zimmermann <dz@planck.security>
2024-05-28 13:32:31 +0200

    CORE-434 Docs

--- src/pEpEngine.h
+++ src/pEpEngine.h
@@ -2192,6 +2192,7 @@ DYNAMIC_API PEP_STATUS has_passphrase(PEP_SESSION session, const char *account, 
  *
  *  @retval PEP_CANNOT_FIND_IDENTITY No errors, none required of the given accounts require a passphrase.
  *  @retval PEP_PASSPHRASE_REQUIRED No errors, at least one account requires a passphrase.
+ *  @retval PEP_ILLEGAL_VALUE Input values are not correct, e.g. NULL values in passphrases.
  */
 DYNAMIC_API PEP_STATUS unlock_keys_with_passphrase(PEP_SESSION session,
     const stringpair_list_t *accounts_with_passphrases,
