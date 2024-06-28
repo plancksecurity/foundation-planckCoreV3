1646b132fa0d52e64c0cf3b4ca913887f16cd0eb
Dirk Zimmermann <dz@planck.security>
2024-06-28 15:59:24 +0200

    Set version to 3.3.32

--- src/pEpEngine_version.h
+++ src/pEpEngine_version.h
@@ -37,13 +37,14 @@
 // 15.05.2024/DZ - bump version to 3.3.29
 // 05.06.2024/DZ - bump version to 3.3.30
 // 10.06.2024/DZ - bump version to 3.3.31
+// 28.06.2024/DZ - bump version to 3.3.32
 
 #ifndef _PEP_ENGINE_VERSION_H_
 #define _PEP_ENGINE_VERSION_H_
 
 #define PEP_ENGINE_VERSION_MAJOR          3
 #define PEP_ENGINE_VERSION_MINOR          3
-#define PEP_ENGINE_VERSION_PATCH          31
+#define PEP_ENGINE_VERSION_PATCH          32
 
 // #define PEP_ENGINE_VERSION_RC             4   /* This is a Release Candidate. */
 
@@ -52,6 +53,6 @@
 /* PEP_ENGINE_VERSION_BRANCH_SUFFIX not defined. */
 
 /* The main version string for the user. */
-#define PEP_ENGINE_VERSION "3.3.31"
+#define PEP_ENGINE_VERSION "3.3.32"
 
 #endif /* #ifndef _PEP_ENGINE_VERSION_H_ */
