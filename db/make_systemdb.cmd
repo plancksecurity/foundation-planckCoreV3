265d1fa6dffd466aac652aba9dce1df72a6a34ea
David Lanzendörfer <dl@planck.security>
2024-07-18 13:52:41 -0700

    Use proper platform tool

--- db/make_systemdb.cmd
+++ db/make_systemdb.cmd
@@ -1,7 +1,7 @@
 @rem this file is in the Public Domain
 @echo off
 
-SET SQL=%1\vcpkg\installed\x64-windows\tools\sqlite3.exe
+SET SQL=%1\vcpkg\installed\%vcpkg_platform%\tools\sqlite3.exe
 if exist system.db del /f system.db
 %SQL% -batch system.db < create_system_db.sql
 for %%c in (*.csv) do echo %%c && echo .import "%%c" wordlist | %SQL% -batch -csv system.db
