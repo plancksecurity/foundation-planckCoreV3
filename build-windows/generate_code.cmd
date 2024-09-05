03d3ec8a627933338047dac63e17e7a70b43be30
David Lanzendörfer <dl@planck.security>
2024-09-05 03:01:41 -0700

    Tidying up generator script

--- build-windows/generate_code.cmd
+++ build-windows/generate_code.cmd
@@ -8,17 +8,14 @@ SET engine_directory=%current_directory:~0,-15%
 ECHO %engine_directory%
 
 :: Create the system.db
-PUSHD %engine_directory%\db
-CALL make_systemdb %1
-IF NOT EXIST "%ProgramData%\pEp" MKDIR "%ProgramData%\pEp"
-DEL /F /Q "%ProgramData%\pEp\system.db"
-MOVE system.db "%ProgramData%\pEp\system.db"
+rem PUSHD %engine_directory%\db
+rem CALL make_systemdb %1
+rem IF NOT EXIST "%ProgramData%\pEp" MKDIR "%ProgramData%\pEp"
+rem DEL /F /Q "%ProgramData%\pEp\system.db"
+rem MOVE system.db "%ProgramData%\pEp\system.db"
 
 :: Generate code in ...\PlanckCoreV3\codegen
-CD ..\codegen
-
-:: Generate code in ...\PlanckCoreV3\codegen
-CD ..\..\PlanckCoreV3\codegen
+PUSHD %1\PlanckCoreV3\codegen
 
 :: Generate the Sync code
 IF NOT EXIST generated MKDIR generated
@@ -68,21 +65,21 @@ rem DISTRIBUTION = distribution keyreset managedgroup exploration echo
 rem SYNC  = sync keysync trustsync groupsync
 rem STORAGE = storage messagestorage
 
-..\..\Tools\asn1c\bin\asn1c -S ../../Tools/asn1c/share/asn1c -gen-PER -fincludes-quoted -fcompound-names -pdu=auto pEp.asn1 sync.asn1 keysync.asn1 trustsync.asn1 groupsync.asn1 distribution.asn1 keyreset.asn1 managedgroup.asn1 exploration.asn1
+SET ASN1C=%1\Tools\asn1c\bin\asn1c -S %1/Tools/asn1c/share/asn1c -gen-PER -fincludes-quoted -fcompound-names -pdu=auto 
+
+%ASN1C% pEp.asn1 sync.asn1 keysync.asn1 trustsync.asn1 groupsync.asn1 distribution.asn1 keyreset.asn1 managedgroup.asn1 exploration.asn1
 IF %ERRORLEVEL% NEQ 0 GOTO end
 type nul >> "Sync.c"
-..\..\Tools\asn1c\bin\asn1c -S ../../Tools/asn1c/share/asn1c -gen-PER -fincludes-quoted -fcompound-names -pdu=auto pEp.asn1 distribution.asn1 keyreset.asn1 managedgroup.asn1 exploration.asn1
+%ASN1C% pEp.asn1 distribution.asn1 keyreset.asn1 managedgroup.asn1 exploration.asn1
 IF %ERRORLEVEL% NEQ 0 GOTO end
 type nul >> "Distribution.c"
 del /q ReceiverRating.c.* ReceiverRating.h.*
-..\..\Tools\asn1c\bin\asn1c -S ../../Tools/asn1c/share/asn1c -gen-PER -fincludes-quoted -fcompound-names -pdu=auto storage.asn1 messagestorage.asn1 pEp.asn1
+%ASN1C% storage.asn1 messagestorage.asn1 pEp.asn1
 IF %ERRORLEVEL% NEQ 0 GOTO end
 type nul >> "Storage.c"
-..\..\Tools\asn1c\bin\asn1c -S ../../Tools/asn1c/share/asn1c -gen-PER -fincludes-quoted -fcompound-names -pdu=auto pEp.asn1 message.asn1
+%ASN1C% pEp.asn1 message.asn1
 IF %ERRORLEVEL% NEQ 0 GOTO end
 type nul >> "ASN1Message.c"
-
-
 
 DEL *-sample.c
 
